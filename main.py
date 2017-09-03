bae7cc16005c20961c4f6273674e653ae3526d46
Raoul Wols <raoulwols@gmail.com>
2017-09-03 11:20:21 +0200

    Add textDocument/willSave and textDocument/willSaveWaitUntil
    For willSaveWaitUntil, it is required to *wait* in the on_pre_save ST callback
    in order to synchronize formatting&saving. For this reason the functionality
    of a request has been enhanced with a blocking mode (by default set to False)
    that blocks the requesting thread until the server responds. A timeout of six
    seconds is in place in case the server doesn't respond.

--- main.py
+++ main.py
@@ -145,6 +145,10 @@ class Request:
     def documentSymbols(cls, params):
         return Request("textDocument/documentSymbol", params)
 
+    @classmethod
+    def willSaveWaitUntil(cls, params):
+        return Request("textDocument/willSaveWaitUntil", params)
+
     def __repr__(self):
         return self.method + " " + str(self.params)
 
@@ -182,6 +186,10 @@ class Notification:
     @classmethod
     def didClose(cls, params):
         return Notification("textDocument/didClose", params)
+
+    @classmethod
+    def willSave(cls, params):
+        return Notification("textDocument/willSave", params)
 
     @classmethod
     def exit(cls):
@@ -362,6 +370,10 @@ class Client(object):
         self.request_id = 0
         self.handlers = {}  # type: Dict[int, Callable]
         self.capabilities = {}  # type: Dict[str, Any]
+        self._lock = threading.Lock()
+        self._condition = threading.Condition(self._lock)
+        self._blocking_request_id = -1
+        self._scratch_response = None
 
     def set_capabilities(self, capabilities):
         self.capabilities = capabilities
@@ -372,12 +384,29 @@ class Client(object):
     def get_capability(self, capability):
         return self.capabilities.get(capability)
 
-    def send_request(self, request: Request, handler: 'Callable'):
+    def send_request(self, request: Request, handler: 'Callable', blocking=False):
         self.request_id += 1
-        debug('request {}: {} '.format(self.request_id, request.method))
-        if handler is not None:
-            self.handlers[self.request_id] = handler
-        self.send_payload(request.to_payload(self.request_id))
+        debug('request', self.request_id, ':', request.method, ', blocking =', blocking)
+        if blocking:
+            with self._condition:
+                self._blocking_request_id = self.request_id
+                self.send_payload(request.to_payload(self.request_id))
+                if not self._condition.wait_for(lambda: self._scratch_response is not None, 6):
+                    printf("Oops, this took too long... (timeout of 6 seconds)")
+                elif self._scratch_response == "ERROR":
+                    printf("Got an error back :(")
+                elif self._scratch_response == "EMPTY":
+                    printf("Got an empty result back.")
+                    handler(None)
+                else:
+                    printf("Got a well-behaved result back.")
+                    handler(self._scratch_response)
+                self._blocking_request_id = -1
+                self._scratch_response = None
+        else:
+            if handler is not None:
+                self.handlers[self.request_id] = handler
+            self.send_payload(request.to_payload(self.request_id))
 
     def send_notification(self, notification: Notification):
         debug('notify: ' + notification.method)
@@ -420,9 +449,8 @@ class Client(object):
                     payload = None
                     try:
                         payload = json.loads(content)
-                        limit = min(len(content), 200)
                         if payload.get("method") != "window/logMessage":
-                            debug("got json: ", content[0:limit])
+                            debug("got json: ", content)
                     except IOError:
                         printf("Got a non-JSON payload: ", content)
                         continue
@@ -432,6 +460,10 @@ class Client(object):
                             error = payload['error']
                             debug("got error: ", error)
                             sublime.status_message(error.get('message'))
+                            if self._blocking_request_id >= 0:
+                                self._scratch_response = "ERROR"
+                                with self._condition:
+                                    self._condition.notify()
                         elif "method" in payload:
                             if "id" in payload:
                                 self.request_handler(payload)
@@ -472,11 +504,16 @@ class Client(object):
     def response_handler(self, response):
         try:
             handler_id = int(response.get("id"))  # dotty sends strings back :(
-            result = response.get('result', None)
-            if (self.handlers[handler_id]):
-                self.handlers[handler_id](result)
+            if self._blocking_request_id == handler_id:
+                self._scratch_response = response.get("result", "EMPTY")
+                with self._condition:
+                    self._condition.notify()
             else:
-                debug("No handler found for id" + response.get("id"))
+                result = response.get('result', None)
+                if (self.handlers[handler_id]):
+                    self.handlers[handler_id](result)
+                else:
+                    debug("No handler found for id", handler_id)
         except Exception as e:
             debug("error handling response", handler_id)
             raise
@@ -1871,6 +1908,28 @@ class CloseListener(sublime_plugin.EventListener):
 
 
 class SaveListener(sublime_plugin.EventListener):
+
+    def on_pre_save(self, view):
+        if not is_supported_view(view):
+            return
+        client = client_for_view(view)
+        if not client:
+            return
+        params = {
+            "textDocument": {
+                "uri": filename_to_uri(view.file_name()),
+                "reason": 1
+            }
+        }
+        format_on_save = sublime.load_settings("LSP.sublime-settings").get("format_on_save", True)
+        can_do_formatting = client.has_capability("documentFormattingProvider")
+        if not can_do_formatting or not format_on_save:
+            client.send_notification(Notification.willSave(params))
+        else:
+            client.send_request(Request.willSaveWaitUntil(params),
+                                lambda response: view.run_command("lsp_apply_document_edit", {"changes": response}),
+                                blocking=True)
+
     def on_post_save_async(self, view):
         if is_supported_view(view):
             # debug("on_post_save_async", view.file_name())

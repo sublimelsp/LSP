030f8e3d357955b010bf9a7285df7af2c55aa3b9
Ryan McCue <me@ryanmccue.info>
2017-09-09 18:13:18 +1000

    Use one project path instead of common prefix (#81)
    * Use first project path instead of common prefix
    * Implement os.path.commonpath ourselves

--- main.py
+++ main.py
@@ -570,10 +570,20 @@ def get_project_path(window: sublime.Window) -> 'Optional[str]':
     """
     if len(window.folders()):
         folder_paths = window.folders()
-        return os.path.commonprefix(folder_paths)
+        return folder_paths[0]
     else:
         debug("Couldn't determine project directory")
         return None
+
+
+def get_common_parent(paths: 'List[str]') -> str:
+    """
+    Get the common parent directory of multiple paths.
+
+    Python 3.5+ includes os.path.commonpath which does this, however Sublime
+    currently embeds Python 3.3.
+    """
+    return os.path.commonprefix([path + '/' for path in paths]).rstrip('/')
 
 
 def is_in_workspace(window: sublime.Window, file_path: str) -> bool:
@@ -581,7 +591,7 @@ def is_in_workspace(window: sublime.Window, file_path: str) -> bool:
     if workspace_path is None:
         return False
 
-    common_dir = os.path.commonprefix([workspace_path, file_path])
+    common_dir = get_common_parent([workspace_path, file_path])
     return workspace_path == common_dir
 
 

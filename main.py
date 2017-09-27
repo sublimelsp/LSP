060a267c45ac05fec5cc76282b134f797063da8e
Josh Leverette <josh.leverette@utiliflex.com>
2017-09-27 14:06:49 -0400

    and another small fix.

--- main.py
+++ main.py
@@ -1926,13 +1926,13 @@ class TypeAnnotator(object):
         formatted = fn_name + re.sub(" *fn *", "", formatted)
         offset = 0
         if "&mut self" in formatted:
-            formatted = "&mut self." + formatted.replace("&mut self", "", 1).replace("(, ", "", 1)
+            formatted = "&mut self." + formatted.replace("&mut self", "", 1).replace("(, ", "(", 1)
             offset = len("&mut self.")
         elif "&self" in formatted:
-            formatted = "&self." + formatted.replace("&self", "", 1).replace("(, ", "", 1)
+            formatted = "&self." + formatted.replace("&self", "", 1).replace("(, ", "(", 1)
             offset = len("&self.")
         elif "self" in formatted:
-            formatted = "self." + formatted.replace("self", "", 1).replace("(, ", "", 1)
+            formatted = "self." + formatted.replace("self", "", 1).replace("(, ", "(", 1)
             offset = len("self.")
         region = Range(
             Point.from_text_point(self.view, region.begin() - offset),

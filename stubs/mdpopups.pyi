e086b054b75413bead2396cd5c2e128c354de0c0
Raoul Wols <raoulwols@gmail.com>
2017-10-09 21:43:35 +0200

    Add type annotation to mdpopup stubs

--- stubs/mdpopups.pyi
+++ stubs/mdpopups.pyi
@@ -1,19 +1,25 @@
 import sublime
+try:
+    from typing import Callable, Optional
+    assert Callable and Optional
+except ImportError:
+    pass
+
 
 def show_popup(
     view: sublime.View,
     content: str,
     md=True,
-    css=None,
+    css=None,  # type: Optional[str]
     flags=0,
     location=-1,
     max_width=320,
     max_height=240,
-    on_navigate=None,
-    on_hide=None,
-    wrapper_class=None,
-    template_vars=None,
-    template_env_options=None,
+    on_navigate=None,  # type: Optional[Callable]
+    on_hide=None,  # type: Optional[Callable]
+    wrapper_class=None,  # type: Optional[str]
+    template_vars=None,  # type: Optional[dict]
+    template_env_options=None,  # type: Optional[dict]
     nl2br=True,
     allow_code_wrap=False
 ): ...
@@ -22,9 +28,9 @@ def update_popup(
     view: sublime.View,
     content: str,
     md=True,
-    css=None,
-    wrapper_class=None,
-    template_vars=None,
+    css=None,  # type: Optional[str]
+    wrapper_class=None,  # type: Optional[str]
+    template_vars=None,  # type: Optional[str]
     nl2br=True,
     allow_code_wrap=False
 ): ...

0af7827ce31f4e3851d42bb52345846488fb8445
Raoul Wols <r@primef.actor>
2017-10-10 20:10:13 +0200

    Browse function overloads with the up/down arrow keys (with no other stuff) (#162)
    * Browse function overloads with the up/down arrow keys

--- main.py
+++ main.py
@@ -2297,16 +2297,25 @@ class CompletionHandler(sublime_plugin.ViewEventListener):
 
 
 class SignatureHelpListener(sublime_plugin.ViewEventListener):
+
+    css = ".mdpopups .lsp_signature { margin: 4px; } .mdpopups p { margin: 0.1rem; }"
+    wrapper_class = "lsp_signature"
+
     def __init__(self, view):
         self.view = view
-        self.signature_help_triggers = None
+        self._initialized = False
+        self._signature_help_triggers = []  # type: List[str]
+        self._visible = False
+        self._language_id = ""
+        self._signatures = []  # type: List[Any]
+        self._active_signature = -1
 
     @classmethod
     def is_applicable(cls, settings):
         syntax = settings.get('syntax')
         return syntax and is_supported_syntax(syntax)
 
-    def initialize_triggers(self):
+    def initialize(self):
         client = client_for_view(self.view)
         if client:
             signatureHelpProvider = client.get_capability(
@@ -2314,16 +2323,19 @@ class SignatureHelpListener(sublime_plugin.ViewEventListener):
             if signatureHelpProvider:
                 self.signature_help_triggers = signatureHelpProvider.get(
                     'triggerCharacters')
-                return
 
-        self.signature_help_triggers = []
+        config = config_for_scope(self.view)
+        if config:
+            self._language_id = config.languageId
+
+        self._initialized = True
 
     def on_modified_async(self):
         pos = self.view.sel()[0].begin()
         last_char = self.view.substr(pos - 1)
         # TODO: this will fire too often, narrow down using scopes or regex
-        if self.signature_help_triggers is None:
-            self.initialize_triggers()
+        if not self._initialized:
+            self.initialize()
 
         if self.signature_help_triggers:
             if last_char in self.signature_help_triggers:
@@ -2337,40 +2349,87 @@ class SignatureHelpListener(sublime_plugin.ViewEventListener):
                             lambda response: self.handle_response(response, pos))
             else:
                 # TODO: this hides too soon.
-                if self.view.is_popup_visible():
+                if self._visible:
                     self.view.hide_popup()
 
     def handle_response(self, response, point):
         if response is not None:
-            config = config_for_scope(self.view)
-            signatures = response.get("signatures")
-            activeSignature = response.get("activeSignature")
-            debug("got signatures, active is", len(signatures), activeSignature)
-            if len(signatures) > 0 and config:
-                signature = signatures[activeSignature]
-                debug("active signature", signature)
-                formatted = []
-                formatted.append(
-                    "```{}\n{}\n```".format(config.languageId, signature.get('label')))
-                params = signature.get('parameters')
-                if params:
-                    for parameter in params:
-                        paramDocs = parameter.get('documentation')
-                        if paramDocs:
-                            formatted.append("**{}**\n".format(parameter.get('label')))
-                            formatted.append("* *{}*\n".format(paramDocs))
+            self._signatures = response.get("signatures", [])
+            self._active_signature = response.get("activeSignature", -1)
 
-                formatted.append(signature.get('documentation'))
+            if self._signatures:
+                if not 0 <= self._active_signature < len(self._signatures):
+                    debug("activeSignature {} not a valid index for signatures length {}".format(
+                        self._active_signature, len(self._signatures)))
+                    self._active_signature = 0
+            else:
+                if self._active_signature != -1:
+                    debug("activeSignature should be -1 or null when no signatures are returned")
+                    self._active_signature = -1
 
-                mdpopups.show_popup(
-                    self.view,
-                    preserve_whitespace("\n".join(formatted)),
-                    css=".mdpopups .lsp_signature { margin: 4px; } .mdpopups p { margin: 0.1rem; }",
-                    md=True,
-                    flags=sublime.HIDE_ON_MOUSE_MOVE_AWAY,
-                    location=point,
-                    wrapper_class="lsp_signature",
-                    max_width=800)
+            if len(self._signatures) > 0:
+                mdpopups.show_popup(self.view,
+                                    self._build_popup_content(),
+                                    css=self.__class__.css,
+                                    md=True,
+                                    flags=sublime.HIDE_ON_MOUSE_MOVE_AWAY,
+                                    location=point,
+                                    wrapper_class=self.__class__.wrapper_class,
+                                    max_width=800,
+                                    on_hide=self._on_hide)
+                self._visible = True
+
+    def on_query_context(self, key, _, operand, __):
+        if key != "lsp.signature_help":
+            return False  # Let someone else handle this keybinding.
+        elif not self._visible:
+            return False  # Let someone else handle this keybinding.
+        elif len(self._signatures) < 2:
+            return False  # Let someone else handle this keybinding.
+        else:
+            # We use the "operand" for the number -1 or +1. See the keybindings.
+            new_index = self._active_signature + operand
+
+            # clamp signature index
+            new_index = max(0, min(new_index, len(self._signatures) - 1))
+
+            # only update when changed
+            if new_index != self._active_signature:
+                self._active_signature = new_index
+                mdpopups.update_popup(self.view,
+                                      self._build_popup_content(),
+                                      css=self.__class__.css,
+                                      md=True,
+                                      wrapper_class=self.__class__.wrapper_class)
+
+            return True  # We handled this keybinding.
+
+    def _on_hide(self):
+        self._visible = False
+
+    def _build_popup_content(self) -> str:
+        signature = self._signatures[self._active_signature]
+        formatted = []
+
+        if len(self._signatures) > 1:
+            signature_navigation = "**{}** of **{}** overloads (use the arrow keys to navigate):\n".format(
+                str(self._active_signature + 1), str(len(self._signatures)))
+            formatted.append(signature_navigation)
+
+        label = "```{}\n{}\n```".format(self._language_id, signature.get('label'))
+        formatted.append(label)
+
+        params = signature.get('parameters')
+        if params:
+            for parameter in params:
+                paramDocs = parameter.get('documentation', None)
+                if paramDocs:
+                    formatted.append("**{}**\n".format(parameter.get('label')))
+                    formatted.append("* *{}*\n".format(paramDocs))
+        sigDocs = signature.get('documentation', None)
+        if sigDocs:
+            formatted.append(sigDocs)
+        return preserve_whitespace("\n".join(formatted))
 
 
 def get_line_diagnostics(view, point):

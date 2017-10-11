089015304b3d16905a41777ec0491951faf4aad9
Tom van Ommeren <tom.vanommeren@gmail.com>
2017-10-12 00:39:06 +0200

    Extract go to definition

--- __init__.py
+++ __init__.py
@@ -4,4 +4,5 @@ from .features.completion import *
 from .features.diagnostics import *
 from .features.configuration import *
 from .features.formatting import *
+from .features.definition import *
 from .features.hover import *

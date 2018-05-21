250ffd3b719319103fd714143f5a36860e7f8e4e
Niklas Mollenhauer <nikeee@outlook.com>
2018-05-21 22:08:01 +0200

    Fix indentation

--- plugin/core/test_url.py
+++ plugin/core/test_url.py
@@ -5,9 +5,9 @@ import unittest
 class WindowsTests(unittest.TestCase):
 
     @unittest.skipUnless(sys.platform.startswith("win"), "requires Windows")
-	def test_converts_path_to_uri(self):
-		self.assertEqual("file:///C:/dir%20ectory/file.txt", filename_to_uri("c:\\dir ectory\\file.txt"))
+    def test_converts_path_to_uri(self):
+        self.assertEqual("file:///C:/dir%20ectory/file.txt", filename_to_uri("c:\\dir ectory\\file.txt"))
 
     @unittest.skipUnless(sys.platform.startswith("win"), "requires Windows")
-	def test_converts_uri_to_path(self):
-		self.assertEqual("C:\\dir ectory\\file.txt", uri_to_filename("file:///c:/dir ectory/file.txt"))
+    def test_converts_uri_to_path(self):
+        self.assertEqual("C:\\dir ectory\\file.txt", uri_to_filename("file:///c:/dir ectory/file.txt"))

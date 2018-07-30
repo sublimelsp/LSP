1ead5f3891d5bbcaba764f85702656bc5933c4ef
Tom van Ommeren <tom.vanommeren@gmail.com>
2018-07-31 01:13:40 +0200

    Add test for multi-session document sync

--- plugin/core/test_documents.py
+++ plugin/core/test_documents.py
@@ -7,6 +7,7 @@ from .test_session import test_config, TestClient
 import unittest.mock
 from . import test_sublime as test_sublime
 from .test_rpc import TestSettings
+from .types import ClientConfig
 
 try:
     from typing import Any, Dict
@@ -44,3 +45,24 @@ class WindowDocumentHandlerTests(unittest.TestCase):
         events.publish("view.on_activated_async", view)
         self.assertFalse(handler.has_document_state(__file__))
         self.assertEqual(len(client._notifications), 0)
+
+    def test_sends_did_open_to_multiple_sessions(self):
+        events = Events()
+        view = TestView(__file__)
+        window = TestWindow([[view]])
+        view.set_window(window)
+        handler = WindowDocumentHandler(test_sublime, TestSettings(), window, events)
+        client = TestClient()
+        session = create_session(test_config, "", dict(), TestSettings(),
+                                 bootstrap_client=client)
+        client2 = TestClient()
+        test_config2 = ClientConfig("test2", [], None, ["source.test"], ["Test.sublime-syntax"], "test")
+        session2 = create_session(test_config2, "", dict(), TestSettings(),
+                                  bootstrap_client=client2)
+
+        handler.add_session(session)
+        handler.add_session(session2)
+        events.publish("view.on_activated_async", view)
+        self.assertTrue(handler.has_document_state(__file__))
+        self.assertEqual(len(client._notifications), 1)
+        self.assertEqual(len(client2._notifications), 1)

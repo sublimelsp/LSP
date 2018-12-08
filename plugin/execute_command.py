580a5005669f2fb2eae15cca921ca7f594623c02
Ayoub Benali <a.benali@dcmn.com>
2018-12-08 22:14:35 +0100

    log response and show popup in case of error

--- plugin/execute_command.py
+++ plugin/execute_command.py
@@ -1,6 +1,8 @@
+import sublime
 from .core.registry import client_for_view, LspTextCommand
 from .core.settings import client_configs
 from .core.protocol import Request
+from .core.logging import debug
 
 try:
     from typing import List, Optional, Dict, Any
@@ -22,8 +24,13 @@ class LspExecuteCommand(LspTextCommand):
         if len(self._commands) > 0:
             self.view.window().show_quick_panel(self._commands, self._on_done)
 
-    def _handle_response(self, response: 'Optional[Any]') -> None:
+    def _handle_response(self, command: str, response: 'Optional[Any]') -> None:
+        debug("response for command {}: {}".format(command, response))
         pass
+
+    def _handle_error(self, command: str, error: 'Dict[str, Any]') -> None:
+        msg = "command {} failed. Reason: {}".format(command, error.get("message", "none provided by server :("))
+        self.view.show_popup(msg, sublime.HIDE_ON_MOUSE_MOVE_AWAY)
 
     def _on_done(self, index: int) -> None:
         if index > -1:
@@ -33,4 +40,6 @@ class LspExecuteCommand(LspTextCommand):
                 request = {
                     "command": command
                 }
-                client.send_request(Request.executeCommand(request), self._handle_response)
+                client.send_request(Request.executeCommand(request),
+                                    lambda reponse: self._handle_response(command, reponse),
+                                    lambda error: self._handle_error(command, error))

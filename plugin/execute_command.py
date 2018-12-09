bbc76884a9a9495901365716d2279b0d57232593
Ayoub Benali <a.benali@dcmn.com>
2018-12-10 00:17:11 +0100

    use CommandNameInputHandler

--- plugin/execute_command.py
+++ plugin/execute_command.py
@@ -1,4 +1,5 @@
 import sublime
+import sublime_plugin
 from .core.registry import client_for_view, LspTextCommand
 from .core.settings import client_configs
 from .core.protocol import Request
@@ -6,10 +7,29 @@ from .core.logging import debug
 from .core.rpc import Client
 
 try:
-    from typing import List, Optional, Dict, Any
-    assert List and Optional and Dict and Any
+    from typing import List, Optional, Dict, Any, Tuple
+    assert List and Optional and Dict and Any, Tuple
 except ImportError:
     pass
+
+
+class CommandNameInputHandler(sublime_plugin.ListInputHandler):
+    def __init__(self, view):
+        super(CommandNameInputHandler, self).__init__()
+        self.view = view
+        client = client_for_view(self.view)
+        if client:
+            self._commands = []  # type: List[Tuple[str, Tuple[str, Dict[str, Any]]]]
+            for config in client_configs.all:
+                for command in config.commands:
+                    x = (command.name, (command.name, command.args))
+                    self._commands.append(x)
+
+    def list_items(self):
+        return self._commands
+
+    def placeholder(self):
+        return self._commands[0]
 
 
 class LspExecuteCommand(LspTextCommand):
@@ -17,20 +37,14 @@ class LspExecuteCommand(LspTextCommand):
         super().__init__(view)
 
     def run(self, edit, command_name: 'Optional[str]'=None, command_args: 'Dict[str, Any]'=dict()) -> None:
+        print("###### got command_name " + str(command_name))
+        print("###### got command_args " + str(command_args))
         client = client_for_view(self.view)
-        if client:
-            if command_name:
-                self._send_command(client, command_name, command_args)
-            else:
-                self._command_names = []   # type: List[str]
-                self._command_args = dict()   # type: Dict[str, Dict[str, Any]]
-                for config in client_configs.all:
-                    for command in config.commands:
-                        self._command_names.append(command.name)
-                        self._command_args[command.name] = command.args
+        if client and command_name:
+            self._send_command(client, command_name, command_args)
 
-                if len(self._command_names) > 0:
-                    self.view.window().show_quick_panel(self._command_names, lambda i: self._on_done(client, i))
+    def input(self, args):
+        return None if 'command_name' and 'command_args' in args else CommandNameInputHandler(self.view)
 
     def _handle_response(self, command: str, response: 'Optional[Any]') -> None:
         debug("response for command {}: {}".format(command, response))
@@ -49,8 +63,8 @@ class LspExecuteCommand(LspTextCommand):
                             lambda reponse: self._handle_response(command_name, reponse),
                             lambda error: self._handle_error(command_name, error))
 
-    def _on_done(self, client: Client, index: int) -> None:
-        if index > -1:
-            command = self._command_names[index]
-            args = self._command_args.get(command, dict())
-            self._send_command(client, command, args)
+    # def _on_done(self, client: Client, index: int) -> None:
+    #     if index > -1:
+    #         command = self._command_names[index]
+    #         args = self._command_args.get(command, dict())
+    #         self._send_command(client, command, args)

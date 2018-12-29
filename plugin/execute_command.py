eacbdfff1e1ad8dbe060456aa486d5e9df303e79
Ayoub Benali <ayoub-benali@users.noreply.github.com>
2018-12-30 00:13:03 +0100

    Allow user to trigger custom commands (#474)
    * Allow user to register custom commands
    
    * add types
    
    * inherit from LspTextCommand
    
    * add test case
    
    * move LspExecuteCommand to its own file
    
    * add execute_command to boot
    
    * log response and show popup in case of error
    
    * create Command class and read and forward command args to the LSP server
    
    * fix test
    
    * add return type to constructor
    
    * minor updates
    
    * make the run method take a command name and its arguments optionally for key binding
    
    * use CommandNameInputHandler
    
    * remove type hint and detect when command_name is a list
    
    * add test case
    
    * add test case
    
    * move test to test_configs
    
    * add test case for execute command
    
    * Allow user to register custom commands
    
    * add types
    
    * inherit from LspTextCommand
    
    * add test case
    
    * move LspExecuteCommand to its own file
    
    * add execute_command to boot
    
    * log response and show popup in case of error
    
    * create Command class and read and forward command args to the LSP server
    
    * fix test
    
    * add return type to constructor
    
    * minor updates
    
    * make the run method take a command name and its arguments optionally for key binding
    
    * use CommandNameInputHandler
    
    * remove type hint and detect when command_name is a list
    
    * add test case
    
    * add test case
    
    * move test to test_configs
    
    * add test case for execute command
    
    * remove commands from client config.
    Commands now can be triggered via key binding or custom commands
    
    * add 'workspace/executeCommand' to mock client responses
    
    * fix test and remove debug print
    
    * make flake happy
    
    * add a wait
    
    * increase time
    
    * try time.sleep
    
    * Reduce PR changes to minimal before merge
    
    * Add missing newline

---
+++ plugin/execute_command.py
@@ -0,0 +1,41 @@
+import sublime
+from .core.registry import client_for_view, LspTextCommand
+from .core.protocol import Request
+from .core.logging import debug
+from .core.rpc import Client
+
+try:
+    from typing import List, Optional, Dict, Any, Tuple
+    assert List and Optional and Dict and Any, Tuple
+except ImportError:
+    pass
+
+
+class LspExecuteCommand(LspTextCommand):
+    def __init__(self, view):
+        super().__init__(view)
+
+    def run(self, edit, command_name=None, command_args=None) -> None:
+        client = client_for_view(self.view)
+        if client and command_name:
+            self.view.window().status_message("Running command {}".format(command_name))
+            self._send_command(client, command_name, command_args)
+
+    def _handle_response(self, command: str, response: 'Optional[Any]') -> None:
+        msg = "command {} completed".format(command)
+        if response:
+            msg += "with response: {}".format(response)
+        sublime.message_dialog(msg)
+
+    def _handle_error(self, command: str, error: 'Dict[str, Any]') -> None:
+        msg = "command {} failed. Reason: {}".format(command, error.get("message", "none provided by server :("))
+        sublime.message_dialog(msg)
+
+    def _send_command(self, client: Client, command_name: str, command_args: 'Dict[str, Any]') -> None:
+        request = {
+            "command": command_name,
+            "args": command_args
+        }
+        client.send_request(Request.executeCommand(request),
+                            lambda reponse: self._handle_response(command_name, reponse),
+                            lambda error: self._handle_error(command_name, error))

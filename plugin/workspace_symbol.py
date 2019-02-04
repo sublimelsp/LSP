2fcaa2a746a277b99a067b81dca4888f0db3cb5a
Ayoub Benali <a.benali@dcmn.com>
2019-02-04 09:10:08 +0100

    change implementation

--- plugin/workspace_symbol.py
+++ plugin/workspace_symbol.py
@@ -1,6 +1,5 @@
 import sublime_plugin
 import sublime
-import time
 from .core.protocol import Request
 from .core.registry import client_for_view, LspTextCommand
 from .core.url import uri_to_filename
@@ -14,84 +13,54 @@ except ImportError:
     pass
 
 
-class SymbolListHandler(sublime_plugin.ListInputHandler):
-    def __init__(self, symbols):
-        self._symbols = symbols
-        self._items = list(map(lambda s: (self._format(s), s), self._symbols))
-
-    def _format(self, s: 'Dict[str, Any]') -> str:
-        file_name = os.path.basename(s['location']['uri'])
-        name = "{} - {} -- {}".format(s['name'], s.get('containerName', ""), file_name)
-        return name
-
-    def list_items(self):
-        return self._items
-
-    def preview(self, s):
-        return format_symbol_kind(s["kind"])
-
-
 class QueryHandler(sublime_plugin.TextInputHandler):
-    def __init__(self, view):
-        self.view = view
-        self.client = client_for_view(self.view)
-        self._symbols = []  # type: 'List[Dict]'
-        self._completed_request = False
-        self._request_time = 0
 
-    def _timeout(self) -> bool:
-        return self._request_time > 3
-
-    def _handle_response(self, symbols: 'Optional[List[Dict[str, Any]]]') -> None:
-        if symbols:
-            self._symbols = symbols
-            self._completed_request = True
-
-    def _handle_error(self, error: 'Dict[str, Any]') -> None:
-        reason = error.get("message", "none provided by server :(")
-        msg = "command 'workspace/symbol' failed. Reason: {}".format(reason)
-        self._completed_request = True
-        sublime.error_message(msg)
-
-    def validate(self, txt):
+    def validate(self, txt) -> bool:
         return txt != ""
-
-    def confirm(self, txt):
-        self._completed_request = False
-        self._request_time = 0
-        request = Request.workspaceSymbol({"query": txt})
-        if self.client:
-            self.client.send_request(request, self._handle_response, self._handle_error)
-
-        # wait for request to complete
-        while not self._completed_request or not self._timeout:
-            time.sleep(0.25)
-            self._request_time += 0.25
-
-        return len(self._symbols) != 0
 
     def placeholder(self):
         return "Symbol"
-
-    def next_input(self, args):
-        return SymbolListHandler(self._symbols)
 
 
 class LspWorkspaceSymbolsCommand(LspTextCommand):
     def __init__(self, view):
         super().__init__(view)
 
+    def _format(self, s: 'Dict[str, Any]') -> str:
+        file_name = os.path.basename(s['location']['uri'])
+        symbol_kind = format_symbol_kind(s["kind"])
+        name = "{} ({}) - {} -- {}".format(s['name'], symbol_kind, s.get('containerName', ""), file_name)
+        return name
+
+    def _open_file(self, symbols, index):
+        if index != -1:
+            symbol = symbols[index]
+            start = symbol['location']['range']['start']
+            file_name = uri_to_filename(symbol['location']['uri'])
+            encoded_file_name = "{}:{}:{}".format(file_name, start['line'], start['character'])
+            print("window " + str(self.view.window()))
+            print(str(self.view.name()))
+            self.view.window().open_file(encoded_file_name, sublime.ENCODED_POSITION)
+
+    def _handle_response(self, matches: 'Optional[List[Dict[str, Any]]]') -> None:
+        if matches:
+            choices = list(map(lambda s: self._format(s), matches))
+            self.view.window().show_quick_panel(choices, lambda i: self._open_file(matches, i))
+
+    def _handle_error(self, error: 'Dict[str, Any]') -> None:
+        reason = error.get("message", "none provided by server :(")
+        msg = "command 'workspace/symbol' failed. Reason: {}".format(reason)
+        sublime.error_message(msg)
+
     def is_enabled(self, event=None):
         return self.has_client_with_capability('workspaceSymbolProvider')
 
     def input(self, args):
-        return QueryHandler(self.view)
+        return QueryHandler()
 
-    def run(self, edit=None, symbol_list_handler: 'Optional[Dict[str, Any]]'=None,
-            query_handler: 'Optional[str]'=None) -> None:
-
-        if symbol_list_handler:
-                start = symbol_list_handler['location']['range']['start']
-                file_name = uri_to_filename(symbol_list_handler['location']['uri'])
-                encoded_file_name = "{}:{}:{}".format(file_name, start['line'], start['character'])
-                self.view.window().open_file(encoded_file_name, sublime.ENCODED_POSITION)
+    def run(self, edit, query_handler: str = None) -> None:
+        if query_handler:
+            request = Request.workspaceSymbol({"query": query_handler})
+            client = client_for_view(self.view)
+            if client:
+                client.send_request(request, self._handle_response, self._handle_error)

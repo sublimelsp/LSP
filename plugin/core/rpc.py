a0a246b5e5a082677da6ea1e51f52d5ce10f1b85
Andreas Haferburg <andreas.haferburg@gmx.de>
2019-03-31 00:44:28 +0100

    We don't like upper case here

--- plugin/core/rpc.py
+++ plugin/core/rpc.py
@@ -92,7 +92,7 @@ class Client(object):
             self._response_handlers[self.request_id] = (handler, error_handler)
             self.send_payload(request.to_payload(self.request_id))
         else:
-            debug('Unable to send', request.method)
+            debug('unable to send', request.method)
             if error_handler is not None:
                 error_handler()
 
@@ -101,7 +101,7 @@ class Client(object):
             debug(' --> ' + notification.method)
             self.send_payload(notification.to_payload())
         else:
-            debug('Unable to send', notification.method)
+            debug('unable to send', notification.method)
 
     def send_response(self, response: Response) -> None:
         self.send_payload(response.to_payload())
@@ -120,7 +120,7 @@ class Client(object):
         self._transport_fail_handler = handler
 
     def handle_transport_failure(self) -> None:
-        debug('Transport failed')
+        debug('transport failed')
         self.transport = None
         if self._transport_fail_handler is not None:
             self._transport_fail_handler()

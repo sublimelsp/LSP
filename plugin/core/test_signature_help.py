f1f9b322203b5cfa94a23cef65154c6725feefa7
Tom van Ommeren <tom.vanommeren@gmail.com>
2019-05-02 15:30:59 +0200

    Move parsing and formatting of signature to core with some tests

---
+++ plugin/core/test_signature_help.py
@@ -0,0 +1,72 @@
+from .signature_help import create_signature_help, SignatureHelp, get_documentation
+from .types import Settings
+import unittest
+
+language_id = "asdf"
+settings = Settings()
+signature = {
+    'label': 'foo_bar(value: int) -> None',
+    'parameters': [{
+        'label': 'value'
+    }]
+}  # type: dict
+
+
+SUBLIME_SINGLE_SIGNATURE = """```asdf
+foo_bar(value: int) -> None
+```
+"""
+
+VSCODE_SINGLE_SIGNATURE = """<div class="highlight"><pre>
+foo_bar(<span style="font-weight: bold; text-decoration: underline">value</span>: int) -&gt; None
+</pre></div>"""
+
+
+class GetDocumentationTests(unittest.TestCase):
+
+    def test_absent(self):
+        self.assertIsNone(get_documentation({}))
+
+    def test_is_str(self):
+        self.assertEqual(get_documentation({'documentation': 'str'}), 'str')
+
+    def test_is_dict(self):
+        self.assertEqual(get_documentation({'documentation': {'value': 'value'}}), 'value')
+
+
+class CreateSignatureHelpTests(unittest.TestCase):
+
+    def test_none(self):
+        self.assertIsNone(create_signature_help(None, language_id, settings))
+
+    def test_empty(self):
+        self.assertIsNone(create_signature_help({}, language_id, settings))
+
+    def test_default_indices(self):
+
+        help = create_signature_help({"signatures": [signature]}, language_id, settings)
+
+        self.assertIsNotNone(help)
+        if help:
+            self.assertEqual(help._active_signature, 0)
+            self.assertEqual(help._active_parameter, -1)
+
+
+class SublimeSignatureHelpTests(unittest.TestCase):
+
+    def test_single_signature(self):
+        help = SignatureHelp([signature], language_id)
+        self.assertIsNotNone(help)
+        if help:
+            content = help.build_popup_content()
+            self.assertEqual(content, SUBLIME_SINGLE_SIGNATURE)
+
+
+class VsCodeSignatureHelpTests(unittest.TestCase):
+
+    def test_single_signature(self):
+        help = SignatureHelp([signature], language_id, highlight_parameter=True)
+        self.assertIsNotNone(help)
+        if help:
+            content = help.build_popup_content()
+            self.assertEqual(content, VSCODE_SINGLE_SIGNATURE)

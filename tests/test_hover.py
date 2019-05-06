dbf228de57a4327f5045a0a27cc82e93da1864d7
Tom van Ommeren <tom.vanommeren@gmail.com>
2019-05-06 11:14:18 +0200

    Improve in-sublime testing (#585)
    * Restore completion tests, fix issues using global state
    
    * Update test for default completion settings
    
    * Use a base class for all text document testing

--- tests/test_hover.py
+++ tests/test_hover.py
@@ -1,14 +1,5 @@
-from unittesting import DeferrableTestCase
-import sublime
-from os.path import dirname
 from LSP.plugin.hover import _test_contents
-from LSP.plugin.core.types import ClientConfig, ClientStates, LanguageConfig
-from LSP.plugin.core.test_session import MockClient
-from LSP.plugin.core.sessions import Session
-from LSP.plugin.core.registry import windows  # , session_for_view
-from LSP.plugin.core.settings import client_configs
-
-test_file_path = dirname(__file__) + "/testfile.txt"
+from setup import TextDocumentTestCase
 
 ORIGINAL_CONTENT = """Hello Wrld"""
 
@@ -19,41 +10,17 @@ EXPECTED_CONTENT = """<dom-module id="some-thing">
 </dom-module>"""
 
 
-class LspHoverCommandTests(DeferrableTestCase):
-
-    def setUp(self):
-        self.view = sublime.active_window().open_file(test_file_path)
+class LspHoverCommandTests(TextDocumentTestCase):
 
     def test_hover_info(self):
+        self.client.responses['textDocument/hover'] = {"contents": "greeting"}
 
         yield 100  # wait for file to be open
         self.view.run_command('insert', {"characters": ORIGINAL_CONTENT})
-
-        wm = windows.lookup(self.view.window())
-        test_language = LanguageConfig("test", ["text.plain"], ["Plain text"])
-        text_config = ClientConfig("test", [], None, languages=[test_language],)
-        client_configs.add_external_config(text_config)
-        client_configs.update_configs()
-        wm._configs.all.append(text_config)
-
-        session = Session(text_config, dirname(__file__), MockClient())
-        session.state = ClientStates.READY
-        wm._sessions[text_config.name] = session
-
-        # session = session_for_view(self.view)
-        # self.assertIsNotNone(session)
-        # self.assertTrue(session.has_capability('hoverProvider'))
-
         self.view.run_command('lsp_hover', {'point': 3})
 
-        # popup should be visible eventually
-        yield self.view.is_popup_visible()
+        yield 100  # popup should be visible eventually
+        self.assertTrue(self.view.is_popup_visible())
 
         last_content = _test_contents[-1]
         self.assertTrue("greeting" in last_content)
-
-    def tearDown(self):
-        if self.view:
-            self.view.set_scratch(True)
-            self.view.window().focus_view(self.view)
-            self.view.window().run_command("close_file")

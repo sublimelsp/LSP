0607a0d847107b05841d14fc843166fa427479b1
Tom van Ommeren <tom.vanommeren@gmail.com>
2019-05-22 12:46:01 +0200

    Add test for reading in tcp transport

--- plugin/core/test_transports.py
+++ plugin/core/test_transports.py
@@ -1,6 +1,6 @@
 import unittest
 import io
-from .transports import StdioTransport
+from .transports import StdioTransport, TCPTransport
 import time
 
 
@@ -25,11 +25,45 @@ class FakeProcess(object):
         self.returncode = returncode
 
 
+class FakeSocket(object):
+    def __init__(self, data: bytes) -> None:
+        self.data = data
+        self.index = 0
+
+    def recv(self, length: int) -> bytes:
+        slc = self.data[self.index:length]
+        self.index = max(len(self.data), self.index + length)
+        return slc
+
+    def sendall(self, payload: bytes) -> None:
+        pass
+
+
 class StdioTransportTests(unittest.TestCase):
     def test_read_messages(self):
 
         process = FakeProcess()
         t = StdioTransport(process)  # type: ignore
+        self.assertIsNotNone(t)
+        received = []
+
+        def on_receive(msg):
+            received.append(msg)
+
+        def on_close():
+            pass
+
+        t.start(on_receive, on_close)
+        time.sleep(0.01)
+        self.assertEqual(received, ["hello", "world"])
+        t.close()
+
+
+class TCPTransportTests(unittest.TestCase):
+    def test_read_messages(self):
+        sock = FakeSocket(
+            json_rpc_message("hello") + json_rpc_message("world"))
+        t = TCPTransport(sock)
         self.assertIsNotNone(t)
         received = []
 

83d649c9f4f7668cdbb4c988391b30cdbea63182
Raoul Wols <raoulwols@gmail.com>
2019-07-22 20:25:59 +0200

    Compactify goto tests a bit

--- tests/test_goto.py
+++ tests/test_goto.py
@@ -1,8 +1,9 @@
 from setup import TextDocumentTestCase, close_test_view
 import sublime
-from os.path import dirname
+from os.path import dirname, join
 
-TEST_FILE_PATH = dirname(__file__) + "/goto.txt"
+SELFDIR = dirname(__file__)
+TEST_FILE_PATH = join(SELFDIR, 'goto.txt')
 
 RESPONSE = [
     {
@@ -24,14 +25,11 @@ class GotoTestCase(TextDocumentTestCase):
     def setUp(self):
         super().setUp()
         yield 100
-        self.view.run_command('insert', {"characters": 'hello there'})
-        yield 100
+        self.view.run_command('insert', {'characters': 'hello there'})
         self.goto_view = sublime.active_window().open_file(TEST_FILE_PATH)
         yield 100
         self.goto_view.run_command('insert', {'characters': 'foo'})
-        yield 100
         self.view.window().focus_view(self.view)  # go back to first view
-        yield 100
 
     def tearDown(self):
         close_test_view(self.goto_view)
@@ -43,14 +41,14 @@ class GotoTestCase(TextDocumentTestCase):
             # self.fail or self.skipTest?
             # self.fail could become annoying when a pull-request sporadically
             # fails for this reason. I (rwols) think we should use skipTest.
-            self.skipTest("invalid Sublime Text view :(")
+            self.skipTest('invalid Sublime Text view :(')
             return
         filename = view.file_name()
         if not filename:
             # self.fail or self.skipTest?
-            self.skipTest("view.file_name() returned nothing :(")
+            self.skipTest('view.file_name() returned nothing :(')
             return
-        self.assertTrue(filename.endswith("goto.txt"))
+        self.assertIn('goto.txt', filename)
         line1, col1 = view.rowcol(view.sel()[0].a)
         line2, col2 = view.rowcol(view.sel()[0].b)
         self.assertEqual(line1, 0)
@@ -58,26 +56,26 @@ class GotoTestCase(TextDocumentTestCase):
         self.assertEqual(line2, 0)
         self.assertEqual(col2, 2)
 
+    def do_run(self, text_document_request: str, subl_command_suffix: str) -> None:
+        self.client.responses['textDocument/' + text_document_request] = RESPONSE
+        self.view.run_command('lsp_symbol_' + subl_command_suffix)
+
     def test_definition(self):
-        self.client.responses['textDocument/definition'] = RESPONSE
-        self.view.run_command('lsp_symbol_definition')
+        self.do_run('definition', 'definition')
         yield 100
         self.do_common_checks()
 
     def test_type_definition(self):
-        self.client.responses['textDocument/typeDefinition'] = RESPONSE
-        self.view.run_command('lsp_symbol_type_definition')
+        self.do_run('typeDefinition', 'type_definition')
         yield 100
         self.do_common_checks()
 
     def test_declaration(self):
-        self.client.responses['textDocument/declaration'] = RESPONSE
-        self.view.run_command('lsp_symbol_declaration')
+        self.do_run('declaration', 'declaration')
         yield 100
         self.do_common_checks()
 
     def test_implementation(self):
-        self.client.responses['textDocument/implementation'] = RESPONSE
-        self.view.run_command('lsp_symbol_implementation')
+        self.do_run('implementation', 'implementation')
         yield 100
         self.do_common_checks()

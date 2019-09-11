a3d28a897863a0869db132542ebaa28689cae7a8
Tom van Ommeren <tom.vanommeren@gmail.com>
2019-09-11 17:07:56 +0200

    Don't use _project_path for projectless windows
    The "switch project" detection logic was firing accidentally when switching views, causing random session shutdowns. The solution is to store the "fallback" active view path separately (_projectless_root_path) and exclude it from the switch project detection.
    _projectless_root_path will only be set once per window.

--- plugin/core/workspace.py
+++ plugin/core/workspace.py
@@ -11,27 +11,33 @@ from .logging import debug
 
 def get_project_path(window: 'Any') -> 'Optional[str]':
     """
-    Returns the first project folder or the parent folder of the active view
+    Returns the first project folder
     """
     if len(window.folders()):
         folder_paths = window.folders()
         return folder_paths[0]
-    else:
-        view = window.active_view()
-        if view:
-            filename = view.file_name()
-            if filename and os.path.exists(filename):  # https://github.com/tomv564/LSP/issues/644
-                project_path = os.path.dirname(filename)
-                debug("Couldn't determine project directory since no folders are open!",
-                      "Using", project_path, "as a fallback.")
-                return project_path
-            else:
-                debug("Couldn't determine project directory since no folders are open",
-                      "and the current file isn't saved on the disk.")
-                return None
+    return None
+
+
+def get_active_view_path(window: 'Any') -> 'Optional[str]':
+    """
+    Returns the path containing the active view, if any.
+    """
+    view = window.active_view()
+    if view:
+        filename = view.file_name()
+        if filename and os.path.exists(filename):  # https://github.com/tomv564/LSP/issues/644
+            project_path = os.path.dirname(filename)
+            debug("Couldn't determine project directory since no folders are open!",
+                  "Using", project_path, "as a fallback.")
+            return project_path
         else:
-            debug("No view is active in current window")
-            return None  # https://github.com/tomv564/LSP/issues/219
+            debug("Couldn't determine project directory since no folders are open",
+                  "and the current file isn't saved on the disk.")
+            return None
+    else:
+        debug("No view is active in current window")
+        return None  # https://github.com/tomv564/LSP/issues/219
 
 
 def get_common_parent(paths: 'List[str]') -> str:

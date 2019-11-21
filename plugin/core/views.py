13159844e100b52b2be7efafb8a0a84d24080508
Predrag Nikolic <predrag.nikolic@codetri.be>
2019-11-21 21:56:29 +0100

    Move the function up in file, just to trigger travis

--- plugin/core/views.py
+++ plugin/core/views.py
@@ -4,6 +4,23 @@ import linecache
 from .protocol import Point, Range
 
 
+def get_line(window: 'sublime.Window', file_name: str, row: int) -> str:
+    '''
+    Get the line from the buffer if the view is open, else get line from linecache.
+    row - is 0 based. If you want to get the first line, you should pass 0.
+    '''
+    view = window.find_open_file(file_name)
+
+    if view:
+        # get from buffer
+        point = view.text_point(row, 0)
+        return view.substr(view.line(point)).strip()
+    else:
+        # get from linecache
+        # linecache row is not 0 based, so we increment it by 1 to get the correct line.
+        return linecache.getline(file_name, row + 1).strip()
+
+
 def point_to_offset(point: Point, view: sublime.View) -> int:
     return view.text_point(point.row, point.col)
 
@@ -21,20 +38,3 @@ def region_to_range(view: sublime.View, region: sublime.Region) -> 'Range':
         offset_to_point(view, region.begin()),
         offset_to_point(view, region.end())
     )
-
-
-def get_line(window: 'sublime.Window', file_name: str, row: int) -> str:
-    '''
-    Get the line from the buffer if the view is open, else get line from linecache.
-    row - is 0 based. If you want to get the first line, you should pass 0.
-    '''
-    view = window.find_open_file(file_name)
-
-    if view:
-        # get from buffer
-        point = view.text_point(row, 0)
-        return view.substr(view.line(point)).strip()
-    else:
-        # get from linecache
-        # linecache row is not 0 based, so we increment it by 1 to get the correct line.
-        return linecache.getline(file_name, row + 1).strip()

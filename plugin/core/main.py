e34e08d5383cc4e9174c1315be7bf0cc02eca6e0
Martin Staffa <mjstaffa@gmail.com>
2019-12-01 21:35:52 +0100

    remove the condition for init display; remove last ref to show_status_message

--- plugin/core/main.py
+++ plugin/core/main.py
@@ -23,8 +23,7 @@ def startup() -> None:
     popups.load_css()
     windows.set_diagnostics_ui(DiagnosticsPresenter)
     load_handlers()
-    if settings.show_view_status:
-        sublime.status_message("LSP initialized")
+    sublime.status_message("LSP initialized")
     start_active_window()
 
 

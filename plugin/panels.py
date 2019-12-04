91ad2c7924a15053535fb3b13a374a99f14b9471
Raoul Wols <raoulwols@gmail.com>
2019-12-04 19:30:58 +0100

    Rework lsp_toggle_panel command

--- plugin/panels.py
+++ plugin/panels.py
@@ -1,20 +1,15 @@
 from .core.windows import ensure_server_panel
 from .diagnostics import ensure_diagnostics_panel
-from sublime import error_message
 from sublime_lib import Panel
 from sublime_plugin import WindowCommand
 
-assert ensure_server_panel
-assert ensure_diagnostics_panel
 
-
-class LspTogglePanel(WindowCommand):
+class LspTogglePanelCommand(WindowCommand):
     def run(self, panel_type: str) -> None:
-        if not isinstance(panel_type, str):
+        if panel_type == "diagnostics":
+            ensure_diagnostics_panel(self.window)
+        elif panel_type == "server":
+            ensure_server_panel(self.window)
+        else:
             return
-        ensure_func = globals().get("ensure_{}_panel".format(panel_type), None)
-        if not callable(ensure_func):
-            error_message('There is no panel of type "{}"'.format(panel_type))
-            return
-        ensure_func(self.window)
         Panel(self.window, "output.{}".format(panel_type)).toggle_visibility()

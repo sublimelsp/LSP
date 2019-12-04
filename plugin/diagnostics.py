a4017989c999937ca39f6a397d93fc9d0888006c
Raoul Wols <raoulwols@gmail.com>
2019-12-04 19:47:03 +0100

    Revert "Convert te panel handling in diagnostics.py to using a sublime_lib.OutputPanel"
    This reverts commit 558f48918ac636393eb770d9c4fb317cd6761ab2.

--- plugin/diagnostics.py
+++ plugin/diagnostics.py
@@ -18,7 +18,6 @@ if MYPY:
     assert Any and List and Dict and Callable and Optional and Tuple
 else:
     Protocol = object  # type: ignore
-    from sublime_lib import OutputPanel
 
 
 diagnostic_severity_names = {
@@ -304,35 +303,36 @@ class StatusBarSummary(DiagnosticsUpdateWalk):
             active_view.set_status('lsp_errors_warning_count', count)
 
 
-class DiagnosticOutputPanel(DiagnosticsUpdateWalk, OutputPanel):
+class DiagnosticOutputPanel(DiagnosticsUpdateWalk):
     def __init__(self, window: sublime.Window) -> None:
         self._window = window
+        self._to_render = []  # type: List[str]
         self._panel = ensure_diagnostics_panel(self._window)
-        OutputPanel.__init__(self, self._window, "diagnostics", force_writes=True, follow_cursor=True)
 
     def begin(self) -> None:
         self._base_dir = windows.lookup(self._window).get_project_path()
-        self._file_content = []  # type: List[str]
-        self._panel.settings().set("result_base_dir", self._base_dir)
-        assert self._panel, "must have a panel now!"
-        self.clear()
+        self._to_render = []
+        self._file_content = ""
 
     def begin_file(self, file_path: str) -> None:
-        self._file_content.clear()
+        self._file_content = ""
 
     def diagnostic(self, diagnostic: Diagnostic) -> None:
         if diagnostic.severity <= settings.show_diagnostics_severity_level:
-            self._file_content.append(self.format_diagnostic(diagnostic))
+            item = self.format_diagnostic(diagnostic)
+            self._file_content += item + "\n"
 
     def end_file(self, file_path: str) -> None:
         if self._file_content:
             panel_file_path = os.path.relpath(file_path, self._base_dir) if self._base_dir else file_path
-            self.print(" ◌ {}:".format(panel_file_path))
-            for content in self._file_content:
-                self.print(content)
+            self._to_render.append(" ◌ {}:\n{}".format(panel_file_path, self._file_content))
 
     def end(self) -> None:
-        pass
+        assert self._panel, "must have a panel now!"
+        self._panel.settings().set("result_base_dir", self._base_dir)
+        self._panel.set_read_only(False)
+        self._panel.run_command("lsp_update_panel", {"characters": "\n".join(self._to_render)})
+        self._panel.set_read_only(True)
 
     def format_diagnostic(self, diagnostic: Diagnostic) -> str:
         location = "{:>8}:{:<4}".format(

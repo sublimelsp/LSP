f9c990de6a183f769ceea7403b05ddc64692696a
Raoul Wols <raoulwols@gmail.com>
2019-12-19 19:36:30 +0100

    Just write two separate commands for the panel togglers

--- plugin/panels.py
+++ plugin/panels.py
@@ -1,24 +1,23 @@
-from .core.logging import printf
 from .core.main import ensure_server_panel
 from .diagnostics import ensure_diagnostics_panel
 from .core.panels import PanelName
 from sublime_plugin import WindowCommand
+from sublime import Window
 
 
-class LspTogglePanelCommand(WindowCommand):
-    def run(self, panel_type: str) -> None:
-        if panel_type == PanelName.Diagnostics:
-            ensure_diagnostics_panel(self.window)
-        elif panel_type == PanelName.LanguageServers:
-            ensure_server_panel(self.window)
-        else:
-            return
+def toggle_output_panel(window: Window, panel_type: str) -> None:
         panel_name = "output.{}".format(panel_type)
-        command = "{}_panel".format("hide" if self.window.active_panel() == panel_name else "show")
-        self.window.run_command(command, {"panel": panel_name})
+        command = "{}_panel".format("hide" if window.active_panel() == panel_name else "show")
+        window.run_command(command, {"panel": panel_name})
+
+
+class LspToggleServerPanelCommand(WindowCommand):
+    def run(self) -> None:
+        ensure_server_panel(self.window)
+        toggle_output_panel(self.window, PanelName.LanguageServers)
 
 
 class LspShowDiagnosticsPanelCommand(WindowCommand):
     def run(self) -> None:
-        printf("lsp_show_diagnostics_panel is deprecated, use lsp_toggle_panel instead (see the keybindings)")
-        self.window.run_command("lsp_toggle_panel", {"panel_type": PanelName.Diagnostics})
+        ensure_diagnostics_panel(self.window)
+        toggle_output_panel(self.window, PanelName.Diagnostics)

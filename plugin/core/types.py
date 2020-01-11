14759ba6b36b238598808946988a0176fc9da131
Raoul Wols <raoulwols@gmail.com>
2020-01-11 12:25:52 +0100

    Remove single quotes from typpes in types.py

--- plugin/core/types.py
+++ plugin/core/types.py
@@ -1,10 +1,4 @@
-try:
-    from typing_extensions import Protocol
-    from typing import Optional, List, Callable, Dict, Any, Iterator
-    assert Optional and List and Callable and Dict and Any and Iterator
-except ImportError:
-    pass
-    Protocol = object  # type: ignore
+from .typing import Optional, List, Dict, Any, Iterator, Protocol
 
 
 class Settings(object):
@@ -44,20 +38,8 @@ class ClientStates(object):
     STOPPING = 2
 
 
-def syntax_language(config: 'ClientConfig', syntax: str) -> 'Optional[LanguageConfig]':
-    for language in config.languages:
-        for lang_syntax in language.syntaxes:
-            if lang_syntax == syntax:
-                return language
-    return None
-
-
-def config_supports_syntax(config: 'ClientConfig', syntax: str) -> bool:
-    return bool(syntax_language(config, syntax))
-
-
 class LanguageConfig(object):
-    def __init__(self, language_id: str, scopes: 'List[str]', syntaxes: 'List[str]') -> None:
+    def __init__(self, language_id: str, scopes: List[str], syntaxes: List[str]) -> None:
         self.id = language_id
         self.scopes = scopes
         self.syntaxes = syntaxes
@@ -66,18 +48,18 @@ class LanguageConfig(object):
 class ClientConfig(object):
     def __init__(self,
                  name: str,
-                 binary_args: 'List[str]',
-                 tcp_port: 'Optional[int]',
-                 scopes: 'List[str]' = [],
-                 syntaxes: 'List[str]' = [],
-                 languageId: 'Optional[str]' = None,
-                 languages: 'List[LanguageConfig]' = [],
+                 binary_args: List[str],
+                 tcp_port: Optional[int],
+                 scopes: List[str] = [],
+                 syntaxes: List[str] = [],
+                 languageId: Optional[str] = None,
+                 languages: List[LanguageConfig] = [],
                  enabled: bool = True,
                  init_options: dict = dict(),
                  settings: dict = dict(),
                  env: dict = dict(),
-                 tcp_host: 'Optional[str]' = None,
-                 tcp_mode: 'Optional[str]' = None) -> None:
+                 tcp_host: Optional[str] = None,
+                 tcp_mode: Optional[str] = None) -> None:
         self.name = name
         self.binary_args = binary_args
         self.tcp_port = tcp_port
@@ -92,23 +74,32 @@ class ClientConfig(object):
         self.env = env
 
 
-class ViewLike(Protocol):
-    def __init__(self) -> None:
-        pass
+def syntax_language(config: ClientConfig, syntax: str) -> Optional[LanguageConfig]:
+    for language in config.languages:
+        for lang_syntax in language.syntaxes:
+            if lang_syntax == syntax:
+                return language
+    return None
 
-    def file_name(self) -> 'Optional[str]':
+
+def config_supports_syntax(config: ClientConfig, syntax: str) -> bool:
+    return bool(syntax_language(config, syntax))
+
+
+class ViewLike(Protocol):
+    def file_name(self) -> Optional[str]:
         ...
 
-    def window(self) -> 'Optional[Any]':  # WindowLike
+    def window(self) -> Optional[Any]:  # WindowLike
         ...
 
     def buffer_id(self) -> int:
         ...
 
-    def substr(self, region: 'Any') -> str:
+    def substr(self, region: Any) -> str:
         ...
 
-    def settings(self) -> 'Any':  # SettingsLike
+    def settings(self) -> Any:  # SettingsLike
         ...
 
     def size(self) -> int:
@@ -117,13 +108,13 @@ class ViewLike(Protocol):
     def set_status(self, key: str, status: str) -> None:
         ...
 
-    def sel(self) -> 'Any':
+    def sel(self) -> Any:
         ...
 
-    def score_selector(self, region: 'Any', scope: str) -> int:
+    def score_selector(self, region: Any, scope: str) -> int:
         ...
 
-    def run_command(self, command_name: str, command_args: 'Dict[str, Any]') -> None:
+    def run_command(self, command_name: str, command_args: Dict[str, Any]) -> None:
         ...
 
 
@@ -134,10 +125,10 @@ class WindowLike(Protocol):
     def is_valid(self) -> bool:
         ...
 
-    def folders(self) -> 'List[str]':
+    def folders(self) -> List[str]:
         ...
 
-    def find_open_file(self, path: str) -> 'Optional[ViewLike]':
+    def find_open_file(self, path: str) -> Optional[ViewLike]:
         ...
 
     def num_groups(self) -> int:
@@ -149,22 +140,22 @@ class WindowLike(Protocol):
     def active_view_in_group(self, group: int) -> ViewLike:
         ...
 
-    def project_data(self) -> 'Optional[dict]':
+    def project_data(self) -> Optional[dict]:
         ...
 
-    def project_file_name(self) -> 'Optional[str]':
+    def project_file_name(self) -> Optional[str]:
         ...
 
-    def active_view(self) -> 'Optional[ViewLike]':
+    def active_view(self) -> Optional[ViewLike]:
         ...
 
     def status_message(self, msg: str) -> None:
         ...
 
-    def views(self) -> 'List[ViewLike]':
+    def views(self) -> List[ViewLike]:
         ...
 
-    def run_command(self, command_name: str, command_args: 'Dict[str, Any]') -> None:
+    def run_command(self, command_name: str, command_args: Dict[str, Any]) -> None:
         ...
 
 
@@ -175,16 +166,16 @@ class ConfigRegistry(Protocol):
     def is_supported(self, view: ViewLike) -> bool:
         ...
 
-    def scope_configs(self, view: ViewLike, point: 'Optional[int]' = None) -> 'Iterator[ClientConfig]':
+    def scope_configs(self, view: ViewLike, point: Optional[int] = None) -> Iterator[ClientConfig]:
         ...
 
-    def syntax_configs(self, view: ViewLike, include_disabled: bool = False) -> 'List[ClientConfig]':
+    def syntax_configs(self, view: ViewLike, include_disabled: bool = False) -> List[ClientConfig]:
         ...
 
     def syntax_supported(self, view: ViewLike) -> bool:
         ...
 
-    def syntax_config_languages(self, view: ViewLike) -> 'Dict[str, LanguageConfig]':
+    def syntax_config_languages(self, view: ViewLike) -> Dict[str, LanguageConfig]:
         ...
 
     def update(self) -> None:

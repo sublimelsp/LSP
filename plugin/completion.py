87ab9e38701286f3b921ec059cd8aafcd5a8781d
Predrag <idmpepe@gmail.com>
2020-02-04 19:36:59 +0100

    remove auto_complete_selector

--- plugin/completion.py
+++ plugin/completion.py
@@ -115,7 +115,6 @@ class CompletionHandler(LSPViewEventListener):
                 'triggerCharacters') or []
             if trigger_chars:
                 self.register_trigger_chars(session, trigger_chars)
-            self.auto_complete_selector = self.view.settings().get("auto_complete_selector", "") or ""
 
     def _view_language(self, config_name: str) -> Optional[str]:
         languages = self.view.settings().get('lsp_language')

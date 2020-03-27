34ae9e15119736ea0bfd1ee1bb4b27c070ff1c77
Predrag <idmpepe@gmail.com>
2020-03-27 23:58:28 +0100

    use transform_region to restore file contents

--- plugin/core/restore_lines.py
+++ plugin/core/restore_lines.py
@@ -7,15 +7,16 @@ class RestoreLines:
         self.saved_lines = []  # type: List[dict]
 
     def save_lines(self, locations: List[int], view: sublime.View) -> None:
-        # Clear previously saved lines
-        self.clear()
+        change_id = view.change_id()
 
         for point in locations:
-            text = view.substr(view.line(point))
-            row, _col = view.rowcol(point)
+            line = view.line(point)
+            change_region = (line.begin(), line.end())
+            text = view.substr(line)
 
             self.saved_lines.append({
-                "row": row,
+                "change_id": change_id,
+                "change_region": change_region,
                 "text": text,
                 # cursor will be use retore the cursor the te exact position
                 "cursor": point
@@ -33,22 +34,18 @@ class RestoreLines:
         return restore_lines
 
     def restore_lines(self, edit: sublime.Edit, view: sublime.View) -> None:
-        # insert back lines from the bottom to top
-        self.saved_lines.reverse()
-
         # restore lines contents
-        for saved_line in self.saved_lines:
-            current_view_point = view.text_point(saved_line['row'], 0)
-            current_line_region = view.line(current_view_point)
-            view.replace(edit, current_line_region, saved_line['text'])
+        # insert back lines from the bottom to top
+        for saved_line in reversed(self.saved_lines):
+            change_id = saved_line['change_id']
+            begin, end = saved_line['change_region']
+            change_region = sublime.Region(begin, end)
+
+            transform_region = view.transform_region_from(change_region, change_id)
+            view.erase(edit, transform_region)
+            view.insert(edit, transform_region.begin(), saved_line['text'])
 
         # restore old cursor position
         view.sel().clear()
         for saved_line in self.saved_lines:
             view.sel().add(saved_line["cursor"])
-
-        # the lines are restored, clear them
-        self.clear()
-
-    def clear(self) -> None:
-        self.saved_lines = []

2773758de44ad051f0ace93667cfdf33b3c4ad69
Raoul Wols <raoulwols@gmail.com>
2020-04-17 22:56:43 +0200

    Handle incoming notifications/responses during a sync request (#787)
    This seems to work for eslint where textDocument/publishDiagnostics are sent during a textDocument/willSaveWatUntil request.
    
    Tested this with the following js file:
    
    ```javascript
    const a = 1;
    ```
    
    and the following .eslintrc.yaml:
    
    ```yaml
    ---
    env:
      node: true
    parserOptions:
      ecmaVersion: 2018
      sourceType: module
    extends: 'eslint:recommended'
    rules:
      semi: error
    ```
    
    using the LSP-eslint package to set up the eslint language server.
    
    * Run notifications and responses before running the handler for the blocking request
    * Add more test assertions
    * Allow handling errors in synchronous requests
      We mirror the behavior in execute_request of send_request
    * Handle invalid params by passing them as errors
    * Fix tests locking up
    * It is not needed to call start_active_views(), it'll happen automatically.

--- plugin/core/transports.py
+++ plugin/core/transports.py
@@ -229,7 +229,7 @@ class StdioTransport(Transport):
                         content_length = 0
                     state = STATE_HEADERS
 
-            except IOError as err:
+            except (AttributeError, IOError) as err:
                 self.close()
                 exception_log("Failure reading stdout", err)
                 state = STATE_EOF

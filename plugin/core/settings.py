2097d371252d5f0652639daa56269ddbe4b6ace3
Ayoub Benali <a.benali@dcmn.com>
2020-04-18 11:09:36 +0200

    Add experimental_capabilities setting
    This would allow language handlers to define client experimental capabilities

--- plugin/core/settings.py
+++ plugin/core/settings.py
@@ -206,4 +206,5 @@ def update_client_config(config: ClientConfig, settings: dict) -> ClientConfig:
         settings.get("env", config.env),
         settings.get("tcp_host", config.tcp_host),
         settings.get("tcp_mode", config.tcp_mode)
+        settings.get("experimental_capabilities", config.experimental_capabilities)
     )

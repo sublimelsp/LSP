88d67b77d9a852a8099d8d69702bf13b90052586
Raoul Wols <raoulwols@gmail.com>
2020-04-19 22:22:49 +0200

    Rewrite transports

--- plugin/core/transports.py
+++ plugin/core/transports.py
@@ -1,44 +1,29 @@
-from abc import ABCMeta, abstractmethod
-import threading
-import time
-import socket
-from queue import Queue
-import subprocess
 from .logging import exception_log, debug
+from .types import ClientConfig
+from .typing import Dict, Any, Optional, IO, Protocol
+from abc import ABCMeta, abstractmethod
+from queue import Queue
+import json
+import os
+import shutil
+import socket
+import sublime
+import subprocess
+import threading
+import weakref
 
-try:
-    from typing import Callable, Dict, Any, Optional, IO
-    assert Callable and Dict and Any and Optional and subprocess and IO
-except ImportError:
-    pass
 
-
-ContentLengthHeader = b"Content-Length: "
-ContentLengthHeader_len = len(ContentLengthHeader)
 TCP_CONNECT_TIMEOUT = 5
-
-try:
-    from typing import Any, Dict, Callable
-    assert Any and Dict and Callable
-except ImportError:
-    pass
 
 
 class UnexpectedProcessExitError(Exception):
     pass
 
 
-class Transport(object, metaclass=ABCMeta):
-    @abstractmethod
-    def __init__(self) -> None:
-        pass
+class Transport(metaclass=ABCMeta):
 
     @abstractmethod
-    def start(self, on_receive: 'Callable[[str], None]', on_closed: 'Callable[[], None]') -> None:
-        pass
-
-    @abstractmethod
-    def send(self, message: str) -> None:
+    def send(self, payload: Dict[str, Any]) -> None:
         pass
 
     @abstractmethod
@@ -46,224 +31,205 @@ class Transport(object, metaclass=ABCMeta):
         pass
 
 
-STATE_HEADERS = 0
-STATE_CONTENT = 1
-STATE_EOF = 2
-
-StateStrings = {STATE_HEADERS: 'STATE_HEADERS',
-                STATE_CONTENT: 'STATE_CONTENT',
-                STATE_EOF:     'STATE_EOF'}
+def encode(d: Dict[str, Any]) -> bytes:
+    return json.dumps(d, sort_keys=False, check_circular=False, separators=(',', ':')).encode('utf-8')
 
 
-def state_to_string(state: int) -> str:
-    return StateStrings.get(state, '<unknown state: {}>'.format(state))
+def decode(message: bytes) -> Dict[str, Any]:
+    return json.loads(message.decode('utf-8'))
 
 
-def start_tcp_listener(tcp_port: int) -> socket.socket:
-    sock = socket.socket()
-    sock.bind(('', tcp_port))
-    port = sock.getsockname()[1]
-    sock.settimeout(TCP_CONNECT_TIMEOUT)
-    debug('listening on {}:{}'.format('localhost', port))
-    sock.listen(1)
-    return sock
-
-
-def start_tcp_transport(port: int, host: 'Optional[str]' = None) -> 'Transport':
-    start_time = time.time()
-    debug('connecting to {}:{}'.format(host or "localhost", port))
-
-    while time.time() - start_time < TCP_CONNECT_TIMEOUT:
+def content_length(line: bytes) -> Optional[int]:
+    if line.startswith(b'Content-Length: '):
+        _, value = line.split(b'Content-Length: ')
+        value = value.strip()
         try:
-            sock = socket.create_connection((host or "localhost", port))
-            return TCPTransport(sock)
-        except ConnectionRefusedError:
-            pass
-
-    # process.kill()
-    raise Exception("Timeout connecting to socket")
+            return int(value)
+        except ValueError as ex:
+            raise ValueError("Invalid Content-Length header: {}".format(value.decode('ascii'))) from ex
+    return None
 
 
-def build_message(content: str) -> str:
-    content_length = len(content)
-    result = "Content-Length: {}\r\n\r\n{}".format(content_length, content)
-    return result
+class TransportCallbacks(Protocol):
+
+    def on_transport_close(self, exit_code: int, exception: Optional[Exception]) -> None:
+        ...
+
+    def on_payload(self, payload: Dict[str, Any]) -> None:
+        ...
+
+    def on_stderr_message(self, message: str) -> None:
+        ...
 
 
-class TCPTransport(Transport):
-    def __init__(self, socket: 'Any') -> None:
-        self.socket = socket  # type: 'Optional[Any]'
-        self.send_queue = Queue()  # type: Queue[Optional[str]]
+class JsonRpcTransport(Transport):
 
-    def start(self, on_receive: 'Callable[[str], None]', on_closed: 'Callable[[], None]') -> None:
-        self.on_receive = on_receive
-        self.on_closed = on_closed
-        self.read_thread = threading.Thread(target=self.read_socket)
-        self.read_thread.start()
-        self.write_thread = threading.Thread(target=self.write_socket)
-        self.write_thread.start()
+    def __init__(self, name: str, process: subprocess.Popen, socket: Optional[socket.socket], reader: IO[bytes],
+                 writer: IO[bytes], stderr: IO[bytes], callback_object: TransportCallbacks) -> None:
+        self._process = process
+        self._socket = socket
+        self._reader = reader
+        self._writer = writer
+        self._stderr = stderr
+        self._reader_thread = threading.Thread(target=self._read_loop, name='{}-reader'.format(name))
+        self._writer_thread = threading.Thread(target=self._write_loop, name='{}-writer'.format(name))
+        self._stderr_thread = threading.Thread(target=self._stderr_loop, name='{}-stderr'.format(name))
+        self._callback_object = weakref.ref(callback_object)
+        self._send_queue = Queue(0)  # type: Queue[Optional[Dict[str, Any]]]
+        self._reader_thread.start()
+        self._writer_thread.start()
+        self._stderr_thread.start()
+        self._closed = False
 
-    def close(self) -> None:
-        self.send_queue.put(None)  # kill the write thread as it's blocked on send_queue
-        self.socket = None
-        self.on_closed()
-
-    def read_socket(self) -> None:
-        remaining_data = b""
-        is_incomplete = False
-        read_state = STATE_HEADERS
-        content_length = 0
-        while self.socket:
-            is_incomplete = False
-            try:
-                received_data = self.socket.recv(4096)
-            except Exception as err:
-                exception_log("Failure reading from socket", err)
-                self.close()
-                break
-
-            if not received_data:
-                debug("no data received, closing")
-                self.close()
-                break
-
-            data = remaining_data + received_data
-            remaining_data = b""
-
-            while len(data) > 0 and not is_incomplete:
-                if read_state == STATE_HEADERS:
-                    headers, _sep, rest = data.partition(b"\r\n\r\n")
-                    if len(_sep) < 1:
-                        is_incomplete = True
-                        remaining_data = data
-                    else:
-                        for header in headers.split(b"\r\n"):
-                            if header.startswith(ContentLengthHeader):
-                                header_value = header[ContentLengthHeader_len:]
-                                content_length = int(header_value)
-                                read_state = STATE_CONTENT
-                        data = rest
-
-                if read_state == STATE_CONTENT:
-                    # read content bytes
-                    if len(data) >= content_length:
-                        content = data[:content_length]
-                        self.on_receive(content.decode("UTF-8"))
-                        data = data[content_length:]
-                        read_state = STATE_HEADERS
-                    else:
-                        is_incomplete = True
-                        remaining_data = data
-
-    def send(self, content: str) -> None:
-        self.send_queue.put(build_message(content))
-
-    def write_socket(self) -> None:
-        while self.socket:
-            message = self.send_queue.get()
-            if message is None:
-                break
-            else:
-                try:
-                    self.socket.sendall(bytes(message, 'UTF-8'))
-                except Exception as err:
-                    exception_log("Failure writing to socket", err)
-                    self.close()
-
-
-class StdioTransport(Transport):
-    def __init__(self, process: 'subprocess.Popen') -> None:
-        self.process = process  # type: Optional[subprocess.Popen]
-        self.send_queue = Queue()  # type: Queue[Optional[str]]
-
-    def start(self, on_receive: 'Callable[[str], None]', on_closed: 'Callable[[], None]') -> None:
-        self.on_receive = on_receive
-        self.on_closed = on_closed
-        self.write_thread = threading.Thread(target=self.write_stdin)
-        self.write_thread.start()
-        self.read_thread = threading.Thread(target=self.read_stdout)
-        self.read_thread.start()
+    def send(self, payload: Dict[str, Any]) -> None:
+        self._send_queue.put_nowait(payload)
 
     def close(self) -> None:
-        self.process = None
-        self.send_queue.put(None)  # kill the write thread as it's blocked on send_queue
-        self.on_closed()
+        if not self._closed:
+            self._send_queue.put_nowait(None)
+            if self._socket:
+                self._socket.close()
+            self._closed = True
 
-    def _checked_stdout(self) -> 'IO[Any]':
-        if self.process:
-            return self.process.stdout
-        else:
-            raise UnexpectedProcessExitError()
+    def _join_thread(self, t: threading.Thread) -> None:
+        if t.ident == threading.current_thread().ident:
+            return
+        try:
+            t.join(2)
+        except TimeoutError as ex:
+            exception_log("failed to join {} thread".format(t.name), ex)
 
-    def read_stdout(self) -> None:
-        """
-        Reads JSON responses from process and dispatch them to response_handler
-        """
-        running = True
-        pid = self.process.pid if self.process else "???"
-        state = STATE_HEADERS
-        content_length = 0
-        while running and self.process and state != STATE_EOF:
-            running = self.process.poll() is None
-            try:
-                # debug("read_stdout: state = {}".format(state_to_string(state)))
-                if state == STATE_HEADERS:
-                    header = self._checked_stdout().readline()
-                    # debug('read_stdout reads: {}'.format(header))
-                    if not header:
-                        # Truly, this is the EOF on the stream
-                        state = STATE_EOF
-                        break
+    def __del__(self) -> None:
+        self.close()
+        self._join_thread(self._writer_thread)
+        self._join_thread(self._reader_thread)
+        self._join_thread(self._stderr_thread)
 
-                    header = header.strip()
-                    if not header:
-                        # Not EOF, blank line -> content follows
-                        state = STATE_CONTENT
-                    elif header.startswith(ContentLengthHeader):
-                        content_length = int(header[ContentLengthHeader_len:])
-                elif state == STATE_CONTENT:
-                    if content_length > 0:
-                        content = self._checked_stdout().read(content_length)
-                        self.on_receive(content.decode("UTF-8"))
-                        # debug("read_stdout: read and received {} byte message".format(content_length))
-                        content_length = 0
-                    state = STATE_HEADERS
-
-            except (AttributeError, IOError) as err:
-                self.close()
-                exception_log("Failure reading stdout", err)
-                state = STATE_EOF
-                break
-            except UnexpectedProcessExitError:
-                self.close()
-                debug("process became None")
-                state = STATE_EOF
-                break
-        debug("process {} stdout ended {}".format(pid, "(still alive)" if self.process else "(terminated)"))
-        if self.process:
-            # We use the stdout thread to block and wait on the exiting process, or zombie processes may be the result.
-            returncode = self.process.wait()
-            debug("process {} exited with code {}".format(pid, returncode))
-            if returncode != 0:
-                self.close()
-        self.send_queue.put(None)
-
-    def send(self, content: str) -> None:
-        self.send_queue.put(build_message(content))
-
-    def write_stdin(self) -> None:
-        while self.process:
-            message = self.send_queue.get()
-            if message is None:
-                break
-            else:
+    def _read_loop(self) -> None:
+        try:
+            while self._reader:
+                line = self._reader.readline()
+                if not line:
+                    break
                 try:
-                    msgbytes = bytes(message, 'UTF-8')
+                    num_bytes = content_length(line)
+                except ValueError:
+                    continue
+                if num_bytes is None:
+                    continue
+                while line and line.strip():
+                    line = self._reader.readline()
+                if not line:
+                    continue
+                body = self._reader.read(num_bytes)
+                callback_object = self._callback_object()
+                if callback_object:
                     try:
-                        self.process.stdin.write(msgbytes)
-                    except AttributeError:
-                        return
-                    self.process.stdin.flush()
-                except (BrokenPipeError, OSError) as err:
-                    exception_log("Failure writing to stdout", err)
-                    self.close()
+                        callback_object.on_payload(decode(body))
+                    except Exception as ex:
+                        exception_log("Error handling payload", ex)
+                else:
+                    break
+        except (AttributeError, BrokenPipeError):
+            pass
+        except Exception as ex:
+            exception_log("Unexpected exception", ex)
+        self._send_queue.put_nowait(None)
+
+    def _end(self, exception: Optional[Exception]) -> None:
+        exit_code = -10000
+        try:
+            self._process.terminate()
+            exit_code = self._process.wait(2)
+        except (AttributeError, ProcessLookupError):
+            pass
+        except Exception as ex:
+            exception = ex  # TODO: Old captured exception is overwritten
+        callback_object = self._callback_object()
+        if callback_object:
+            callback_object.on_transport_close(exit_code, exception)
+
+    def _write_loop(self) -> None:
+        exception = None  # type: Optional[Exception]
+        try:
+            while self._writer:
+                d = self._send_queue.get()
+                if d is None:
+                    break
+                body = encode(d)
+                self._writer.writelines((
+                    "Content-Length: {}\r\n".format(len(body)).encode('ascii'),
+                    "Content-Type: application/vscode-jsonrpc; charset=utf-8\r\n\r\n".encode('ascii'),
+                    body))
+                self._writer.flush()
+        except (BrokenPipeError, AttributeError):
+            pass
+        except Exception as ex:
+            exception = ex
+        self._end(exception)
+
+    def _stderr_loop(self) -> None:
+        try:
+            while self._stderr:
+                message = self._stderr.readline().decode('utf-8', 'replace').rstrip()
+                if not message:
+                    break
+                callback_object = self._callback_object()
+                if callback_object:
+                    callback_object.on_stderr_message(message)
+                else:
+                    break
+        except (BrokenPipeError, AttributeError):
+            pass
+        except Exception as ex:
+            exception_log('unexpected exception type in stderr loop', ex)
+        self._send_queue.put_nowait(None)
+
+
+def create_transport(config: ClientConfig, cwd: str, window: sublime.Window,
+                     callback_object: TransportCallbacks) -> JsonRpcTransport:
+    variables = window.extract_variables()
+    args = [sublime.expand_variables(os.path.expanduser(arg), variables) for arg in config.binary_args]
+    env = os.environ.copy()
+    for var, value in config.env.items():
+        env[var] = os.path.expandvars(sublime.expand_variables(value, variables))
+    if config.tcp_port:
+        stdout = subprocess.DEVNULL
+        stdin = subprocess.DEVNULL
+    else:
+        stdout = subprocess.PIPE
+        stdin = subprocess.PIPE
+    if sublime.platform() == "windows":
+        startupinfo = subprocess.STARTUPINFO()  # type: ignore
+        startupinfo.dwFlags |= subprocess.SW_HIDE | subprocess.STARTF_USESHOWWINDOW  # type: ignore
+        executable_arg = args[0]
+        fname, ext = os.path.splitext(executable_arg)
+        if len(ext) < 1:
+            path_to_executable = shutil.which(executable_arg)
+            # what extensions should we append so CreateProcess can find it?
+            # node has .cmd
+            # dart has .bat
+            # python has .exe wrappers - not needed
+            for extension in ['.cmd', '.bat']:
+                if path_to_executable and path_to_executable.lower().endswith(extension):
+                    args[0] = executable_arg + extension
+                    break
+    else:
+        startupinfo = None
+    debug("starting process in", cwd)
+    process = subprocess.Popen(
+        args=args,
+        stdin=stdin,
+        stdout=stdout,
+        stderr=subprocess.PIPE,
+        startupinfo=startupinfo,
+        cwd=cwd)
+    sock = None  # type: Optional[socket.socket]
+    if config.tcp_port:
+        sock = socket.create_connection((None, config.tcp_port), TCP_CONNECT_TIMEOUT)
+        reader = sock.makefile('rwb')  # type: IO[bytes]
+        writer = reader
+    else:
+        reader = process.stdout
+        writer = process.stdin
+    return JsonRpcTransport(config.name, process, sock, reader, writer, process.stderr, callback_object)

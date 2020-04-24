eae9a6db283a9f9f4587c87e037b9db48d04c1b6
Ayoub Benali <ayoub-benali@users.noreply.github.com>
2020-04-24 20:41:43 +0200

    migrate window/showMessageRequest handling to mdpopups.show_popup (#974)
    * migrate window/showMessageRequest handling to mdpopups.show_popup
    
    previously it was handled with show_quick_panel but we couldn't show
    the request message to the user. Until ST4 would allow such a feature
    we resort to mdpopups.show_popup

---
+++ plugin/core/message_request_handler.py
@@ -0,0 +1,80 @@
+from .protocol import Response
+from .rpc import Client
+from .typing import Any, List, Callable
+import mdpopups
+import sublime
+
+
+class MessageRequestHandler():
+    def __init__(self, view: sublime.View, client: Client, request_id: Any, params: dict, source: str) -> None:
+        self.client = client
+        self.request_id = request_id
+        self.request_sent = False
+        self.view = view
+        actions = params.get("actions", [])
+        self.titles = list(action.get("title") for action in actions)
+        self.message = params.get('message', '')
+        self.message_type = params.get('type', 4)
+        self.source = source
+
+    def _send_user_choice(self, href: int = -1) -> None:
+        if not self.request_sent:
+            self.request_sent = True
+            self.view.hide_popup()
+            # when noop; nothing was selected e.g. the user pressed escape
+            param = None
+            index = int(href)
+            if index != -1:
+                param = {"title": self.titles[index]}
+            response = Response(self.request_id, param)
+            self.client.send_response(response)
+
+    def show(self) -> None:
+        show_notification(
+            self.view,
+            self.source,
+            self.message_type,
+            self.message,
+            self.titles,
+            self._send_user_choice,
+            self._send_user_choice
+        )
+
+
+def message_content(source: str, message_type: int, message: str, titles: List[str]) -> str:
+    formatted = []
+    icons = {
+        1: '❗',
+        2: '⚠️',
+        3: 'ℹ️',
+        4: '📝'
+    }
+    icon = icons.get(message_type, '')
+    formatted.append("<h2>{}</h2>".format(source))
+    formatted.append("<p class='message'>{} {}</p>".format(icon, message))
+
+    buttons = []
+    for idx, title in enumerate(titles):
+        buttons.append("<a href='{}'>{}</a>".format(idx, title))
+
+    formatted.append("<p class='actions'>" + " ".join(buttons) + "</p>")
+
+    return "".join(formatted)
+
+
+def show_notification(view: sublime.View, source: str, message_type: int, message: str, titles: List[str],
+                      on_navigate: Callable, on_hide: Callable) -> None:
+    stylesheet = sublime.load_resource("Packages/LSP/notification.css")
+    contents = message_content(source, message_type, message, titles)
+    mdpopups.show_popup(
+        view,
+        contents,
+        css=stylesheet,
+        md=False,
+        location=-1,
+        wrapper_class='notification',
+        max_width=800,
+        max_height=800,
+        on_navigate=on_navigate,
+        on_hide=on_hide
+    )
--- plugin/core/windows.py
+++ plugin/core/windows.py
@@ -1,6 +1,7 @@
 from .diagnostics import DiagnosticsStorage
 from .edit import parse_workspace_edit
 from .logging import debug
+from .message_request_handler import MessageRequestHandler
 from .protocol import Notification, Response
 from .rpc import Client, SublimeLogger
 from .sessions import Session
@@ -459,20 +460,9 @@ class WindowManager(object):
             debug("window {} added session {}".format(self._window.id(), config.name))
             self._sessions.setdefault(config.name, []).append(session)
 
-    def _handle_message_request(self, params: dict, client: Client, request_id: Any) -> None:
-        actions = params.get("actions", [])
-        titles = list(action.get("title") for action in actions)
-
-        def send_user_choice(index: int) -> None:
-            # when noop; nothing was selected e.g. the user pressed escape
-            result = None
-            if index != -1:
-                result = {"title": titles[index]}
-            response = Response(request_id, result)
-            client.send_response(response)
-
-        if actions:
-            self._sublime.active_window().show_quick_panel(titles, send_user_choice)
+    def _handle_message_request(self, params: dict, source: str, client: Client, request_id: Any) -> None:
+        handler = MessageRequestHandler(self._window.active_view(), client, request_id, params, source)  # type: ignore
+        handler.show()
 
     def restart_sessions(self) -> None:
         self._restarting = True
@@ -521,7 +511,7 @@ class WindowManager(object):
 
         client.on_request(
             "window/showMessageRequest",
-            lambda params, request_id: self._handle_message_request(params, client, request_id))
+            lambda params, request_id: self._handle_message_request(params, session.config.name, client, request_id))
 
         client.on_notification(
             "window/showMessage",

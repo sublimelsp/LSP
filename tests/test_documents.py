402d6660af6fc377926ed39abf422c4000b8056e
Raoul Wols <raoulwols@gmail.com>
2020-05-07 01:46:29 +0200

    Rewrite test_sends_did_open_to_multiple_sessions in terms of the test server

--- tests/test_documents.py
+++ tests/test_documents.py
@@ -1,76 +1,117 @@
-from LSP.plugin.core.protocol import WorkspaceFolder
-from LSP.plugin.core.sessions import create_session
-from LSP.plugin.core.sessions import Session
-from LSP.plugin.core.types import ClientConfig
-from LSP.plugin.core.windows import WindowDocumentHandler
-from LSP.plugin.core.workspace import ProjectFolders
-from os.path import basename
-from test_mocks import MockClient
-from test_mocks import MockConfigs
-from test_mocks import MockSettings
-from test_mocks import MockView
-from test_mocks import MockWindow
-from test_mocks import TEST_CONFIG
-from test_mocks import TEST_LANGUAGE
-import test_sublime
-import unittest
-import unittest.mock
-
-try:
-    from typing import Any, Dict
-    assert Any and Dict and Session
-except ImportError:
-    pass
+from copy import deepcopy
+from LSP.plugin.core.documents import DocumentSyncListener
+from LSP.plugin.core.logging import debug
+from LSP.plugin.core.protocol import Request
+from LSP.plugin.core.registry import windows
+from LSP.plugin.core.types import ClientStates
+from LSP.plugin.core.typing import Any, Generator
+from os.path import join
+from setup import add_config
+from setup import close_test_view
+from setup import expand
+from setup import make_stdio_test_config
+from setup import remove_config
+from setup import TIMEOUT_TIME
+from setup import YieldPromise
+from sublime_plugin import view_event_listeners
+from unittesting import DeferrableTestCase
+import sublime
 
 
-class WindowDocumentHandlerTests(unittest.TestCase):
+class WindowDocumentHandlerTests(DeferrableTestCase):
 
-    def assert_if_none(self, session) -> 'Session':
-        self.assertIsNotNone(session)
-        return session
+    def ensure_document_listener_created(self) -> bool:
+        assert self.view
+        # Bug in ST3? Either that, or CI runs with ST window not in focus and that makes ST3 not trigger some
+        # events like on_load_async, on_activated, on_deactivated. That makes things not properly initialize on
+        # opening file (manager missing in DocumentSyncListener)
+        # Revisit this once we're on ST4.
+        for listener in view_event_listeners[self.view.id()]:
+            if isinstance(listener, DocumentSyncListener):
+                sublime.set_timeout_async(listener.on_activated_async)
+                return True
+        return False
 
-    def test_sends_did_open_to_multiple_sessions(self):
-        view = MockView(__file__)
-        window = MockWindow([[view]])
-        project_path = "/"
-        folders = [WorkspaceFolder.from_path(project_path)]
-        view.set_window(window)
-        workspace = ProjectFolders(window)
-        configs = MockConfigs()
-        handler = WindowDocumentHandler(test_sublime, MockSettings(), window, workspace, configs)
-        client = MockClient()
-        session = self.assert_if_none(
-            create_session(TEST_CONFIG, folders, dict(), MockSettings(),
-                           bootstrap_client=client))
-        client2 = MockClient()
-        test_config2 = ClientConfig(name="test2", binary_args=[], tcp_port=None, languages=[TEST_LANGUAGE])
-        configs.all.append(test_config2)
-        session2 = self.assert_if_none(
-            create_session(test_config2, folders, dict(), MockSettings(),
-                           bootstrap_client=client2))
+    def test_sends_did_open_to_multiple_sessions(self) -> Generator:
+        window = sublime.active_window()
+        self.assertTrue(window)
+        self.config1 = make_stdio_test_config()
+        self.config1.init_options["serverResponse"] = {
+            'capabilities': {
+                'textDocumentSync': {
+                    "openClose": True,
+                    "change": 1,
+                    "save": True
+                },
+            }
+        }
+        self.config2 = deepcopy(self.config1)
+        self.config2.name = "foobar"
+        add_config(self.config1)
+        add_config(self.config2)
+        self.wm = windows.lookup(window)
+        self.wm._configs.all.append(self.config1)
+        self.wm._configs.all.append(self.config2)
+        filename = expand(join("$packages", "LSP", "tests", "testfile.txt"), window)
+        open_view = window.find_open_file(filename)
+        close_test_view(open_view)
+        self.view = window.open_file(filename)
+        yield {"condition": lambda: not self.view.is_loading(), "timeout": TIMEOUT_TIME}
+        self.assertTrue(self.wm._configs.match_view(self.view))
+        # self.init_view_settings()
+        yield {"condition": self.ensure_document_listener_created, "timeout": TIMEOUT_TIME}
+        yield {
+            "condition": lambda: self.wm.get_session(self.config1.name, self.view.file_name()) is not None,
+            "timeout": TIMEOUT_TIME}
+        yield {
+            "condition": lambda: self.wm.get_session(self.config2.name, self.view.file_name()) is not None,
+            "timeout": TIMEOUT_TIME}
+        self.session1 = self.wm.get_session(self.config1.name, self.view.file_name())
+        self.session2 = self.wm.get_session(self.config2.name, self.view.file_name())
+        self.assertIsNotNone(self.session1)
+        self.assertIsNotNone(self.session2)
+        self.assertEqual(self.session1.config.name, self.config1.name)
+        self.assertEqual(self.session2.config.name, self.config2.name)
+        yield {"condition": lambda: self.session1.state == ClientStates.READY, "timeout": TIMEOUT_TIME}
+        yield {"condition": lambda: self.session2.state == ClientStates.READY, "timeout": TIMEOUT_TIME}
+        yield from self.await_message("initialize")
+        yield from self.await_message("initialized")
+        yield from self.await_message("textDocument/didOpen")
+        self.view.run_command("insert", {"characters": "a"})
+        yield from self.await_message("textDocument/didChange")
+        status_string = self.view.get_status("lsp_clients")
+        self.assertEqual(status_string, "TEST, foobar")
+        close_test_view(self.view)
+        yield from self.await_message("textDocument/didClose")
+        remove_config(self.config2)
+        remove_config(self.config1)
 
-        handler.add_session(session)
-        handler.add_session(session2)
-        handler.handle_did_open(view)
-        self.assertTrue(handler.has_document_state(__file__))
-        self.assertEqual(len(client._notifications), 1)
-        self.assertEqual(len(client2._notifications), 1)
+    def doCleanups(self) -> Generator:
+        close_test_view(self.view)
+        try:
+            remove_config(self.config2)
+        except ValueError:
+            pass
+        try:
+            remove_config(self.config1)
+        except ValueError:
+            pass
+        yield from super().doCleanups()
 
-        did_open = client._notifications[0]
-        document = did_open.params["textDocument"]
-        self.assertEqual(document.get("languageId"), "test")
-        self.assertEqual(document.get("text"), "asdf")
-        self.assertIsNotNone(document.get("version"))
+    def await_message(self, method: str) -> Generator:
+        promise1 = YieldPromise()
+        promise2 = YieldPromise()
 
-        did_open2 = client._notifications[0]
-        document2 = did_open2.params["textDocument"]
-        self.assertEqual(document2.get("languageId"), "test")
-        self.assertEqual(document2.get("text"), "asdf")
-        self.assertIsNotNone(document2.get("version"))
-        status_string = view._status.get("lsp_clients")
-        if status_string:
-            status_configs = status_string.split(", ")
-            self.assertIn("test", status_configs)
-            self.assertIn("test2", status_configs)
-            self.assertIn("test", status_configs)
-            self.assertIn("test2", status_configs)
+        def handler1(params: Any) -> None:
+            promise1.fulfill(params)
+
+        def handler2(params: Any) -> None:
+            promise2.fulfill(params)
+
+        def error_handler(params: 'Any') -> None:
+            debug("Got error:", params, "awaiting timeout :(")
+
+        self.session1.client.send_request(Request("$test/getReceived", {"method": method}), handler1, error_handler)
+        self.session2.client.send_request(Request("$test/getReceived", {"method": method}), handler2, error_handler)
+        yield {"condition": promise1, "timeout": TIMEOUT_TIME}
+        yield {"condition": promise2, "timeout": TIMEOUT_TIME}

10c4004be55dd54ff61473093f03e2a90ca867ca
Rafał Chłodnicki <rchl2k@gmail.com>
2020-05-10 22:00:47 +0200

    Support capability argument for sessions_for_view (#1030)
    - Allow passed capability to be in 'dotted' format like 'textDocumentSync.change'
    - Replace some checks to use get_dotted_value to make code more compact
    - Don't set None if registerOptions not provided
    
    The logic of changed code is not necessarily 100% identical to the old
    one (empty dicts will return True now) but I think it shouldn't make
    a difference in practice.

--- plugin/color.py
+++ plugin/color.py
@@ -40,12 +40,11 @@ class LspColorListener(sublime_plugin.ViewEventListener):
         configs = configs_for_scope(self.view)
         if not configs:
             self.initialized = True  # no server enabled, re-open file to activate feature.
-        sessions = list(sessions_for_view(self.view))
+        sessions = list(sessions_for_view(self.view, 'colorProvider'))
         if sessions:
             self.initialized = True
-            if any(session.has_capability('colorProvider') for session in sessions):
-                self.enabled = True
-                self.send_color_request()
+            self.enabled = True
+            self.send_color_request()
         elif not is_retry:
             # session may be starting, try again once in a second.
             sublime.set_timeout_async(lambda: self.initialize(is_retry=True), 1000)

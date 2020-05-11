c23315c734383b9db6f9f8886d348dd4e5d33b58
Predrag <idmpepe@gmail.com>
2020-05-12 00:02:59 +0200

    Hide docs popup on commit completion

--- plugin/completion.py
+++ plugin/completion.py
@@ -214,7 +214,7 @@ class CompletionHandler(LSPViewEventListener):
     def on_post_text_command(self, command: str, args: dict) -> None:
         if not self.view.is_popup_visible():
             return
-        if command in ["hide_auto_complete", "move"] or 'delete' in command:
+        if command in ["hide_auto_complete", "move", "commit_completion"] or 'delete' in command:
             # hide the popup when `esc` or arrows are pressed pressed
             self.view.hide_popup()
 

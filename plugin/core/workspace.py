93a06bc09e111d732a0736ba91f598c2f1c10015
Raoul Wols <raoulwols@gmail.com>
2020-05-30 12:03:04 +0200

    Use the new project callbacks from EventListener
    We can now rely on ST for this.

--- plugin/core/workspace.py
+++ plugin/core/workspace.py
@@ -1,7 +1,7 @@
 from .logging import debug
 from .protocol import WorkspaceFolder
 from .types import WindowLike
-from .typing import List, Optional, Any, Callable
+from .typing import List, Any
 
 
 def is_subpath_of(file_path: str, potential_subpath: str) -> bool:
@@ -13,46 +13,17 @@ class ProjectFolders(object):
 
     def __init__(self, window: WindowLike) -> None:
         self._window = window
-        self.on_changed = None  # type: Optional[Callable[[List[str]], None]]
-        self.on_switched = None  # type: Optional[Callable[[List[str]], None]]
-        self.folders = []  # type: List[str]
-        self._current_project_file_name = self._window.project_file_name()
-        self._set_folders(window.folders())
+        self.folders = self._window.folders()  # type: List[str]
 
-    def update(self) -> None:
-        new_folders = self._window.folders()
-        if set(new_folders) != set(self.folders):
-            is_update = self._can_update_to(new_folders)
-            self._set_folders(new_folders)
-            if is_update:
-                if self.on_changed:
-                    self.on_changed(new_folders)
-            else:
-                if self.on_switched:
-                    self.on_switched(new_folders)
+    def update(self) -> List[WorkspaceFolder]:
+        self.folders = self._window.folders()
+        return get_workspace_folders(self.folders)
 
     def includes_path(self, file_path: str) -> bool:
         if self.folders:
             return any(is_subpath_of(file_path, folder) for folder in self.folders)
         else:
             return True
-
-    def _set_folders(self, folders: List[str]) -> None:
-        self.folders = folders
-
-    def _can_update_to(self, new_folders: List[str]) -> bool:
-        """ Should detect difference between a project switch and a change to folders in the loaded project """
-        if not self.folders:
-            return True
-
-        if self._current_project_file_name and self._window.project_file_name() == self._current_project_file_name:
-            return True
-
-        for folder in self.folders:
-            if folder in new_folders:
-                return True
-
-        return False
 
 
 def get_workspace_folders(folders: List[str]) -> List[WorkspaceFolder]:

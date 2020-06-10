3711cb9066edd1d41972b8f1cc191b23b5f56a2c
Raoul Wols <raoulwols@gmail.com>
2020-06-10 09:49:19 +0200

    Replace a global map of view IDs to phantomset (#1101)
    By storing the phantomset in the listener instead.
    Also separate the parsing and construction of params and the actual
    logic in the listener.
    The place for parsing payloads is in views.py

--- plugin/color.py
+++ plugin/color.py
@@ -1,16 +1,11 @@
 import sublime
-from .core.documents import is_transient_view
-from .core.protocol import Range
 from .core.protocol import Request
 from .core.registry import LSPViewEventListener
 from .core.settings import settings
-from .core.typing import List, Dict, Optional
-from .core.url import filename_to_uri
-from .core.views import range_to_region
+from .core.typing import List, Optional
+from .core.views import document_color_params
+from .core.views import lsp_color_to_phantom
 from .core.windows import debounced
-
-
-color_phantoms_by_view = dict()  # type: Dict[int, sublime.PhantomSet]
 
 
 class LspColorListener(LSPViewEventListener):
@@ -19,16 +14,17 @@ class LspColorListener(LSPViewEventListener):
         self._stored_point = -1
         self.initialized = False
         self.enabled = False
+        self._phantoms = sublime.PhantomSet(self.view, "lsp_color")
+
+    def __del__(self) -> None:
+        self._stored_point = -1  # Prevent a debounced request to alter the phantoms again
+        self._phantoms.update([])
 
     @classmethod
     def is_applicable(cls, view_settings: dict) -> bool:
         if 'colorProvider' in settings.disabled_capabilities:
             return False
         return cls.has_supported_syntax(view_settings)
-
-    @property
-    def phantom_set(self) -> sublime.PhantomSet:
-        return color_phantoms_by_view.setdefault(self.view.id(), sublime.PhantomSet(self.view, "lsp_color"))
 
     def on_activated_async(self) -> None:
         if not self.initialized:
@@ -38,7 +34,7 @@ class LspColorListener(LSPViewEventListener):
         if self.session('colorProvider'):
             self.initialized = True
             self.enabled = True
-            self.send_color_request()
+            self.fire_request()
         elif not is_retry:
             # session may be starting, try again once in a second.
             sublime.set_timeout_async(lambda: self.initialize(is_retry=True), 1000)
@@ -53,53 +49,13 @@ class LspColorListener(LSPViewEventListener):
             current_point = sel[0].begin()
             if self._stored_point != current_point:
                 self._stored_point = current_point
-                debounced(self.send_color_request, 800, lambda: self._stored_point == current_point, async_thread=True)
+                debounced(self.fire_request, 800, lambda: self._stored_point == current_point, async_thread=True)
 
-    def send_color_request(self) -> None:
-        if is_transient_view(self.view):
-            return
-
+    def fire_request(self) -> None:
         session = self.session('colorProvider')
         if session:
-            file_path = self.view.file_name()
-            if file_path:
-                params = {
-                    "textDocument": {
-                        "uri": filename_to_uri(file_path)
-                    }
-                }
-                session.send_request(
-                    Request.documentColor(params),
-                    self.handle_response
-                )
+            session.send_request(Request.documentColor(document_color_params(self.view)), self.handle_response)
 
     def handle_response(self, response: Optional[List[dict]]) -> None:
         color_infos = response if response else []
-        phantoms = []
-        for color_info in color_infos:
-            color = color_info['color']
-            red = color['red'] * 255
-            green = color['green'] * 255
-            blue = color['blue'] * 255
-            alpha = color['alpha']
-
-            content = """
-            <style>html {{padding: 0}}</style>
-            <div style='padding: 0.4em;
-                        margin-top: 0.2em;
-                        border: 1px solid color(var(--foreground) alpha(0.25));
-                        background-color: rgba({}, {}, {}, {})'>
-            </div>""".format(red, green, blue, alpha)
-
-            range = Range.from_lsp(color_info['range'])
-            region = range_to_region(range, self.view)
-
-            phantoms.append(sublime.Phantom(region, content, sublime.LAYOUT_INLINE))
-
-        self.phantom_set.update(phantoms)
-
-
-def remove_color_boxes(view: sublime.View) -> None:
-    phantom_set = color_phantoms_by_view.get(view.id())
-    if phantom_set:
-        phantom_set.update([])
+        self._phantoms.update([lsp_color_to_phantom(self.view, color_info) for color_info in color_infos])

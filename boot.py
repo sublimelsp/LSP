4dae6aca02988be92987c203406e344bb128b804
Rafał Chłodnicki <rchl2k@gmail.com>
2020-07-23 22:06:59 +0200

    Support side-by-side view for "show_symbol_action_links"

--- boot.py
+++ boot.py
@@ -29,6 +29,7 @@ from .plugin.goto import LspSymbolDefinitionCommand
 from .plugin.goto import LspSymbolImplementationCommand
 from .plugin.goto import LspSymbolTypeDefinitionCommand
 from .plugin.hover import LspHoverCommand
+from .plugin.hover import LspRunCommandFromPointCommand
 from .plugin.panels import LspShowDiagnosticsPanelCommand
 from .plugin.panels import LspToggleServerPanelCommand
 from .plugin.references import LspSymbolReferencesCommand

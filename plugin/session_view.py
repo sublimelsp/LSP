c002f6bf37b4ae09b103b5e81aadd4b443e5e6d5
Predrag <idmpepe@gmail.com>
2020-10-11 15:31:25 +0200

    don't mutate data.regions

--- plugin/session_view.py
+++ plugin/session_view.py
@@ -127,13 +127,13 @@ class SessionView:
                 self.view.erase_regions(key)
             elif ((severity <= userprefs().show_diagnostics_severity_level) and
                     (data.icon or flags != (sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE))):
-                def handle_same_regions(region: sublime.Region):
+                def handle_same_regions(region: sublime.Region) -> sublime.Region:
                     # this allows showing diagnostics with same begin and end in the view
                     if region.begin() == region.end():
                         return sublime.Region(region.begin(), region.begin() + 1)
                     return region
-                data.regions = list(map(handle_same_regions, data.regions))
-                self.view.add_regions(key, data.regions, data.scope, data.icon, flags)
+                underline_regions = list(map(handle_same_regions, data.regions))
+                self.view.add_regions(key, underline_regions, data.scope, data.icon, flags)
             else:
                 self.view.erase_regions(key)
         listener = self.listener()

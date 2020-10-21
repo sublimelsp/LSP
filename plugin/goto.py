eef6632ab209da1c24206ac0d681da37738a2fa5
Raoul Wols <raoulwols@gmail.com>
2020-10-21 12:27:03 +0200

    Ensure send_request runs in the async thread for the goto variants

--- plugin/goto.py
+++ plugin/goto.py
@@ -1,5 +1,4 @@
 import sublime
-from .core.logging import debug
 from .core.protocol import Request
 from .core.registry import get_position
 from .core.registry import LspTextCommand
@@ -10,12 +9,7 @@ from .core.views import text_document_position_params
 from .documents import is_at_word
 
 
-def process_response_list(responses: list) -> List[str]:
-    return [location_to_encoded_filename(x) for x in responses]
-
-
 def open_location(window: sublime.Window, location: str, side_by_side: bool = True) -> None:
-    debug("opening location", location)
     flags = sublime.ENCODED_POSITION
     if side_by_side:
         flags |= sublime.ADD_TO_SELECTION_SEMI_TRANSIENT
@@ -45,31 +39,43 @@ class LspGotoCommand(LspTextCommand):
     def is_enabled(self, event: Optional[dict] = None) -> bool:
         return super().is_enabled(event) and is_at_word(self.view, event)
 
-    def run(self, edit: sublime.Edit, event: Optional[dict] = None, side_by_side: bool = False) -> None:
-        session = self.best_session(self.capability)
-        if session:
-            pos = get_position(self.view, event)
-            request = Request(self.method, text_document_position_params(self.view, pos), self.view)
-            session.send_request(request, lambda response: self.handle_response(response, side_by_side))
+    def run(self, _: sublime.Edit, event: Optional[dict] = None, side_by_side: bool = False) -> None:
+        params = text_document_position_params(self.view, get_position(self.view, event))
+
+        def run_async() -> None:
+            session = self.best_session(self.capability)
+            if session:
+                request = Request(self.method, params, self.view)
+                session.send_request(
+                    request,
+                    lambda response: sublime.set_timeout(
+                        lambda: self.handle_response(
+                            response, side_by_side
+                        )
+                    )
+                )
+
+        sublime.set_timeout_async(run_async)
 
     def handle_response(self, response: Any, side_by_side: bool) -> None:
+        if not self.view.is_valid():
+            return
         window = self.view.window()
-        view = self.view
         if window is None:
             return
         if response:
-            if len(view.sel()) > 0:
-                view.run_command("add_jump_record", {"selection": [(r.a, r.b) for r in view.sel()]})
+            if len(self.view.sel()) > 0:
+                self.view.run_command("add_jump_record", {"selection": [(r.a, r.b) for r in self.view.sel()]})
             if isinstance(response, dict):
                 locations = [location_to_encoded_filename(response)]
             else:
-                locations = process_response_list(response)
+                locations = [location_to_encoded_filename(x) for x in response]
             if len(locations) == 1:
                 open_location(window, locations[0], side_by_side)
             elif len(locations) > 1:
                 window.show_quick_panel(
                     items=locations,
-                    on_select=lambda x: select_entry(window, locations, x, view, side_by_side),
+                    on_select=lambda x: select_entry(window, locations, x, self.view, side_by_side),
                     on_highlight=lambda x: highlight_entry(window, locations, x),
                     flags=sublime.KEEP_OPEN_ON_FOCUS_LOST)
             # TODO: can add region here.

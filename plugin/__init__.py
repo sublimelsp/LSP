a6ba4ac2f877bf3f1340bb7b9d632072cb1bed2f
Rafał Chłodnicki <rchl2k@gmail.com>
2020-11-06 21:29:37 +0100

    Export public API

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,1 +1,25 @@
 __version__ = (0, 13, 1)
+
+from .core.protocol import Notification
+from .core.protocol import Request
+from .core.protocol import Response
+from .core.protocol import WorkspaceFolder
+from .core.settings import read_client_config
+from .core.types import ClientConfig
+from .core.types import LanguageConfig
+from .core.url import filename_to_uri
+from .core.url import uri_to_filename
+
+# This is the public API for LSP-* packages
+__all__ = [
+    '__version__',
+    'ClientConfig',
+    'filename_to_uri',
+    'LanguageConfig',
+    'Notification',
+    'Request',
+    'Response',
+    'read_client_config',
+    'uri_to_filename',
+    'WorkspaceFolder',
+]

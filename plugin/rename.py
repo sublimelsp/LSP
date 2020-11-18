9a8717a18245bf493bc374f4fe033f9659801b44
Raoul Wols <raoulwols@gmail.com>
2020-11-18 08:59:33 +0100

    Make renaming more careful (#1461)
    When a server returns text edits for textDocument/rename that span more than
    one file, we'll now show a dialog asking the user to confirm that he/she really
    wants to apply the changes. The user can also select "Dry Run" to show the
    locations of the text edits that will be applied in an output panel.
    
    Resolves #873

--- plugin/rename.py
+++ plugin/rename.py
@@ -1,14 +1,19 @@
-import sublime
-import sublime_plugin
 from .core.edit import apply_workspace_edit
 from .core.edit import parse_workspace_edit
+from .core.edit import TextEdit
+from .core.panels import ensure_panel
+from .core.panels import PanelName
 from .core.protocol import Range
 from .core.protocol import Request
 from .core.registry import get_position
 from .core.registry import LspTextCommand
-from .core.typing import Any, Optional
+from .core.registry import windows
+from .core.typing import Any, Optional, Dict, List
 from .core.views import range_to_region
 from .core.views import text_document_position_params
+import os
+import sublime
+import sublime_plugin
 
 
 class RenameSymbolInputHandler(sublime_plugin.TextInputHandler):
@@ -105,7 +110,18 @@ class LspSymbolRenameCommand(LspTextCommand):
         window = self.view.window()
         if window:
             if response:
-                apply_workspace_edit(window, parse_workspace_edit(response))
+                changes = parse_workspace_edit(response)
+                file_count = len(changes.keys())
+                if file_count > 1:
+                    total_changes = sum(map(len, changes.values()))
+                    message = "Replace {} occurrences across {} files?".format(total_changes, file_count)
+                    choice = sublime.yes_no_cancel_dialog(message, "Replace", "Dry Run")
+                    if choice == sublime.DIALOG_YES:
+                        apply_workspace_edit(window, changes)
+                    elif choice == sublime.DIALOG_NO:
+                        self._render_rename_panel(changes, total_changes, file_count)
+                else:
+                    apply_workspace_edit(window, changes)
             else:
                 window.status_message('Nothing to rename')
 
@@ -126,3 +142,49 @@ class LspSymbolRenameCommand(LspTextCommand):
 
     def on_prepare_error(self, error: Any) -> None:
         sublime.error_message("Rename error: {}".format(error["message"]))
+
+    def _get_relative_path(self, file_path: str) -> str:
+        window = self.view.window()
+        if not window:
+            return file_path
+        base_dir = windows.lookup(window).get_project_path(file_path)
+        if base_dir:
+            return os.path.relpath(file_path, base_dir)
+        else:
+            return file_path
+
+    def _render_rename_panel(self, changes: Dict[str, List[TextEdit]], total_changes: int, file_count: int) -> None:
+        window = self.view.window()
+        if not window:
+            return
+        panel = ensure_rename_panel(window)
+        if not panel:
+            return
+        text = ''
+        for file, file_changes in changes.items():
+            text += '◌ {}:\n'.format(self._get_relative_path(file))
+            for edit in file_changes:
+                start = edit[0]
+                text += '\t{:>8}:{}\n'.format(start[0] + 1, start[1] + 1)
+            # append a new line after each file name
+            text += '\n'
+        base_dir = windows.lookup(window).get_project_path(self.view.file_name() or "")
+        panel.settings().set("result_base_dir", base_dir)
+        panel.run_command("lsp_clear_panel")
+        window.run_command("show_panel", {"panel": "output.rename"})
+        fmt = "{} changes across {} files. Double-click on a row:col number to jump to that location.\n\n{}"
+        panel.run_command('append', {
+            'characters': fmt.format(total_changes, file_count, text),
+            'force': True,
+            'scroll_to_end': False
+        })
+
+
+def ensure_rename_panel(window: sublime.Window) -> Optional[sublime.View]:
+    return ensure_panel(
+        window=window,
+        name=PanelName.Rename,
+        result_file_regex=r"^\s*\S\s+(\S.*):$",
+        result_line_regex=r"^\s*([0-9]+):([0-9]+)\s*$",
+        syntax="Packages/LSP/Syntaxes/Rename.sublime-syntax"
+    )

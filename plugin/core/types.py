520cb76059f516ace691752ae1d0a0b40ea503f2
Raoul Wols <r@primef.actor>
2020-11-23 21:49:16 +0100

    Revert "Add an optional ClientConfig argument to filename_to_uri and uri_to_filename"
    This reverts commit 81a6e6aeb2c3a6aebad59fbd6eb0361301243bd1.

--- plugin/core/types.py
+++ plugin/core/types.py
@@ -2,11 +2,9 @@ from .collections import DottedDict
 from .logging import debug, set_debug_logging
 from .protocol import TextDocumentSyncKindNone
 from .typing import Any, Optional, List, Dict, Generator, Callable, Iterable, Union, Set, Tuple, TypeVar
+from .url import filename_to_uri
+from .url import uri_to_filename
 from threading import RLock
-from urllib.parse import urljoin
-from urllib.parse import urlparse
-from urllib.request import pathname2url
-from urllib.request import url2pathname
 from wcmatch.glob import BRACE
 from wcmatch.glob import globmatch
 from wcmatch.glob import GLOBSTAR
@@ -686,14 +684,10 @@ class ClientConfig:
                 path, mapped = path_map.map_from_local_to_remote(path)
                 if mapped:
                     break
-        return urljoin('file:', pathname2url(path))
+        return filename_to_uri(path)
 
     def map_server_uri_to_client_path(self, uri: str) -> str:
-        if os.name == 'nt':
-            # url2pathname does not understand %3A (VS Code's encoding forced on all servers :/)
-            path = url2pathname(urlparse(uri).path).strip('\\')
-        else:
-            path = url2pathname(urlparse(uri).path)
+        path = uri_to_filename(uri)
         if self.path_maps:
             for path_map in self.path_maps:
                 path, mapped = path_map.map_from_remote_to_local(path)

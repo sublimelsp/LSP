159bc765a18670e9d62cf57bbb2cbe620c6802bf
Rafał Chłodnicki <rchl2k@gmail.com>
2021-02-12 20:25:06 +0100

    Expand also built-in variables in lsp_execute's command arguments

--- plugin/execute_command.py
+++ plugin/execute_command.py
@@ -29,11 +29,9 @@ class LspExecuteCommand(LspTextCommand):
             return sublime.set_timeout_async(run_async)
         session = self.session_by_name(session_name if session_name else self.session_name)
         if session and command_name:
-            if command_args:
-                self._expand_variables(command_args)
             params = {"command": command_name}  # type: ExecuteCommandParams
             if command_args:
-                params["arguments"] = command_args
+                params["arguments"] = self._expand_variables(command_args)
 
             def handle_response(response: Any) -> None:
                 assert command_name
@@ -49,15 +47,16 @@ class LspExecuteCommand(LspTextCommand):
 
             session.execute_command(params, progress=True).then(handle_response)
 
-    def _expand_variables(self, command_args: List[Any]) -> None:
-        region = self.view.sel()[0]
+    def _expand_variables(self, command_args: List[Any]) -> List[Any]:
+        view = self.view  # type: sublime.View
+        region = view.sel()[0]
         for i, arg in enumerate(command_args):
             if arg in ["$document_id", "${document_id}"]:
-                command_args[i] = text_document_identifier(self.view)
+                command_args[i] = text_document_identifier(view)
             if arg in ["$file_uri", "${file_uri}"]:
-                command_args[i] = uri_from_view(self.view)
+                command_args[i] = uri_from_view(view)
             elif arg in ["$selection", "${selection}"]:
-                command_args[i] = self.view.substr(region)
+                command_args[i] = view.substr(region)
             elif arg in ["$offset", "${offset}"]:
                 command_args[i] = region.b
             elif arg in ["$selection_begin", "${selection_begin}"]:
@@ -65,6 +64,7 @@ class LspExecuteCommand(LspTextCommand):
             elif arg in ["$selection_end", "${selection_end}"]:
                 command_args[i] = region.end()
             elif arg in ["$position", "${position}"]:
-                command_args[i] = offset_to_point(self.view, region.b).to_lsp()
+                command_args[i] = offset_to_point(view, region.b).to_lsp()
             elif arg in ["$range", "${range}"]:
-                command_args[i] = region_to_range(self.view, region).to_lsp()
+                command_args[i] = region_to_range(view, region).to_lsp()
+        return sublime.expand_variables(command_args, view.window().extract_variables())

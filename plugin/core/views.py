28dbc2c51b36f120928c69ba2debe27a79e5b72b
Raoul Wols <raoulwols@gmail.com>
2021-02-13 19:36:06 +0100

    Use viewport_extent instead

--- plugin/core/views.py
+++ plugin/core/views.py
@@ -372,7 +372,7 @@ def show_lsp_popup(view: sublime.View, contents: str, location: int = -1, md: bo
         flags=flags,
         location=location,
         wrapper_class=wrapper_class,
-        max_width=view.layout_extent()[0],
+        max_width=view.viewport_extent()[0],
         max_height=1000000,
         on_navigate=on_navigate)
 

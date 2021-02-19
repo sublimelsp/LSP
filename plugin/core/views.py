4610a1b46be9932880679e6587c52ad58ee052bd
jwortmann <jwortmann@outlook.com>
2021-02-19 23:24:25 +0100

    Replace document_highlight_scopes setting with better default scopes (#1585)
    * Replace document_highlight_scopes setting with better default scopes
    * Use region scopes for documentHighlight and remove border for fill style
    * Add back markup scopes for diagnostic severities
    
    This removes the user setting for the scopes used to color highlighted
    regions of the textDocument/documentHighlight request. Users (or color
    scheme authors) can configure their desired style via rules in the color
    scheme instead. To easily allow fine-tuned colors, the more specific
    scopes `markup.highlight.{text|read|write}.lsp` were added to the
    corresponding DocumentHighlightKinds.
    
    Additionally, the `Unknown` kind was removed, because it doesn't exist
    in the specification. Complying with the spec, the default kind `Text`
    is used now in case the highlight kind is omitted in the response.

--- plugin/core/views.py
+++ plugin/core/views.py
@@ -24,11 +24,11 @@ import sublime_plugin
 import tempfile
 
 DIAGNOSTIC_SEVERITY = [
-    # Kind       CSS class   Scope for color     Icon resource
-    ("error",   "errors",   "region.redish",    "Packages/LSP/icons/error.png"),
-    ("warning", "warnings", "region.yellowish", "Packages/LSP/icons/warning.png"),
-    ("info",    "info",     "region.bluish",    "Packages/LSP/icons/info.png"),
-    ("hint",    "hints",    "region.bluish",    "Packages/LSP/icons/info.png"),
+    # Kind       CSS class   Scope for color                        Icon resource
+    ("error",   "errors",   "region.redish markup.error.lsp",      "Packages/LSP/icons/error.png"),
+    ("warning", "warnings", "region.yellowish markup.warning.lsp", "Packages/LSP/icons/warning.png"),
+    ("info",    "info",     "region.bluish markup.info.lsp",       "Packages/LSP/icons/info.png"),
+    ("hint",    "hints",    "region.bluish markup.info.hint.lsp",  "Packages/LSP/icons/info.png"),
 ]
 
 # The scope names mainly come from http://www.sublimetext.com/docs/3/scope_naming.html

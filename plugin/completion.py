6d7ce396e9622a7f97f86e1a9dd49ea9cf34c9b9
Raoul Wols <raoulwols@gmail.com>
2021-03-10 18:26:23 +0100

    Fix completion docs not showing for servers that are not resolve providers

--- plugin/completion.py
+++ plugin/completion.py
@@ -27,14 +27,14 @@ class LspResolveDocsCommand(LspTextCommand):
         if not detail or not documentation:
 
             def run_async() -> None:
-                # To make sure that the detail or documentation fields doesn't exist we need to resove the completion
+                # To make sure that the detail or documentation fields do not exist we need to resolve the completion
                 # item. If those fields appear after the item is resolved we show them in the popup.
                 session = self.session_by_name(session_name, 'completionProvider.resolveProvider')
                 if session:
                     request = Request.resolveCompletionItem(item, self.view)
                     session.send_request_async(request, self.handle_resolve_response_async)
 
-            return sublime.set_timeout_async(run_async)
+            sublime.set_timeout_async(run_async)
         minihtml_content = self.get_content(documentation, detail)
         self.show_popup(minihtml_content)
 

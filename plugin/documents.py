c7ffb666afc1970ec12703485bcfb4c6e255a9dd
husanjun <1466472672@qq.com>
2021-05-27 02:02:45 +0800

    Improve code action annotation UX (#1677)
    If there is exactly one code action available, show the title in the annotation.
    If the annotation is clicked, it is ran immediately, circumventing the
    quick panel.

--- plugin/documents.py
+++ plugin/documents.py
@@ -43,6 +43,7 @@ from functools import partial
 from weakref import WeakSet
 from weakref import WeakValueDictionary
 import functools
+import itertools
 import sublime
 import sublime_plugin
 import webbrowser
@@ -483,8 +484,11 @@ class DocumentSyncListener(sublime_plugin.ViewEventListener, AbstractViewListene
             scope = 'region.yellowish lightbulb.lsp'
             icon = 'Packages/LSP/icons/lightbulb.png'
         else:  # 'annotation'
-            suffix = 's' if action_count > 1 else ''
-            code_actions_link = make_command_link('lsp_code_actions', '{} code action{}'.format(action_count, suffix))
+            if action_count > 1:
+                title = '{} code actions'.format(action_count)
+            else:
+                title = next(itertools.chain.from_iterable(responses.values()))['title']
+            code_actions_link = make_command_link('lsp_code_actions', title, {"commands_by_config": responses})
             annotations = ["<div class=\"actions\">{}</div>".format(code_actions_link)]
             annotation_color = self.view.style_for_scope("region.bluish markup.accent.codeaction.lsp")["foreground"]
         self.view.add_regions(self.CODE_ACTIONS_KEY, regions, scope, icon, flags, annotations, annotation_color)

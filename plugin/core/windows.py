3531a092614aaffb8b6a89903becb3921cd47e99
Raoul Wols <raoulwols@gmail.com>
2021-06-03 00:38:14 +0200

    In an early exit continue handling listeners
    This isn't all that pretty but this function should be async-ified
    anyway.

--- plugin/core/windows.py
+++ plugin/core/windows.py
@@ -315,6 +315,9 @@ class WindowManager(Manager):
                 if cannot_start_reason:
                     config.erase_view_status(initiating_view)
                     message = "cannot start {}: {}".format(config.name, cannot_start_reason)
+                    # Continue with handling pending listeners
+                    self._new_session = None
+                    sublime.set_timeout_async(self._dequeue_listener_async)
                     return self._window.status_message(message)
                 cwd = plugin_class.on_pre_start(self._window, initiating_view, workspace_folders, config)
             config.set_view_status(initiating_view, "starting...")

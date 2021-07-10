4b0790d67378efb3d53b91795e7a1f98cdabe8dc
Raoul Wols <raoulwols@gmail.com>
2021-07-10 11:49:42 +0200

    Fix mypy type issue on windows

--- plugin/core/transports.py
+++ plugin/core/transports.py
@@ -236,12 +236,13 @@ def kill_all_subprocesses() -> None:
             pass
 
 
-def _fixup_startup_args(args: List[str]) -> Any:
+def _fixup_startup_args(args: List[str]) -> Optional[subprocess.STARTUPINFO]:
+    startupinfo = None  # type: Optional[subprocess.STARTUPINFO]
     if sublime.platform() == "windows":
-        startupinfo = subprocess.STARTUPINFO()  # type: ignore
-        startupinfo.dwFlags |= subprocess.SW_HIDE | subprocess.STARTF_USESHOWWINDOW  # type: ignore
+        startupinfo = subprocess.STARTUPINFO()
+        startupinfo.dwFlags |= subprocess.SW_HIDE | subprocess.STARTF_USESHOWWINDOW
         executable_arg = args[0]
-        fname, ext = os.path.splitext(executable_arg)
+        _, ext = os.path.splitext(executable_arg)
         if len(ext) < 1:
             path_to_executable = shutil.which(executable_arg)
             # what extensions should we append so CreateProcess can find it?
@@ -252,8 +253,6 @@ def _fixup_startup_args(args: List[str]) -> Any:
                 if path_to_executable and path_to_executable.lower().endswith(extension):
                     args[0] = executable_arg + extension
                     break
-    else:
-        startupinfo = None
     return startupinfo
 
 

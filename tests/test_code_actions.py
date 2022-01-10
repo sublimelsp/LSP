80cf786aeabc4671617d7922b726773f5132459f
Janos Wortmann <jwortmann@outlook.com>
2022-01-10 18:12:15 +0100

    Fix tests

--- tests/test_code_actions.py
+++ tests/test_code_actions.py
@@ -5,6 +5,7 @@ from LSP.plugin.core.typing import Any, Dict, Generator, List, Tuple, Optional
 from LSP.plugin.core.url import filename_to_uri
 from LSP.plugin.core.views import entire_content
 from LSP.plugin.documents import DocumentSyncListener
+from LSP.plugin.session_view import SessionView
 from LSP.plugin.core.views import versioned_text_document_identifier
 from setup import TextDocumentTestCase
 from test_single_document import TEST_FILE_PATH
@@ -271,7 +272,7 @@ class CodeActionsListenerTestCase(TextDocumentTestCase):
         self.assertEquals(params['range']['end']['line'], 1)
         self.assertEquals(params['range']['end']['character'], 1)
         self.assertEquals(len(params['context']['diagnostics']), 2)
-        annotations_range = self.view.get_regions(DocumentSyncListener.CODE_ACTIONS_KEY)
+        annotations_range = self.view.get_regions(SessionView.CODE_ACTIONS_KEY)
         self.assertEquals(len(annotations_range), 1)
         self.assertEquals(annotations_range[0].a, 3)
         self.assertEquals(annotations_range[0].b, 0)
@@ -293,7 +294,7 @@ class CodeActionsListenerTestCase(TextDocumentTestCase):
         self.assertEquals(params['range']['end']['line'], 1)
         self.assertEquals(params['range']['end']['character'], 1)
         self.assertEquals(len(params['context']['diagnostics']), 0)
-        annotations_range = self.view.get_regions(DocumentSyncListener.CODE_ACTIONS_KEY)
+        annotations_range = self.view.get_regions(SessionView.CODE_ACTIONS_KEY)
         self.assertEquals(len(annotations_range), 1)
         self.assertEquals(annotations_range[0].a, 3)
         self.assertEquals(annotations_range[0].b, 0)

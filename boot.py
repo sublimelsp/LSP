5dfd068a8227076c6cd189af7b827b596425edab
Raoul Wols <raoulwols@gmail.com>
2022-02-15 21:36:48 +0100

    Remove registration of the legacy LanguageHandler class (#1936)

--- boot.py
+++ boot.py
@@ -1,7 +1,6 @@
 import os
 import sublime
 import sublime_plugin
-import weakref
 
 # Please keep this list sorted (Edit -> Sort Lines)
 from .plugin.code_actions import LspCodeActionsCommand
@@ -14,32 +13,25 @@ from .plugin.configuration import LspEnableLanguageServerGloballyCommand
 from .plugin.configuration import LspEnableLanguageServerInProjectCommand
 from .plugin.core.collections import DottedDict
 from .plugin.core.css import load as load_css
-from .plugin.core.handlers import LanguageHandler
 from .plugin.core.logging import exception_log
 from .plugin.core.open import opening_files
 from .plugin.core.panels import destroy_output_panels
 from .plugin.core.panels import LspClearPanelCommand
 from .plugin.core.panels import LspUpdatePanelCommand
 from .plugin.core.panels import LspUpdateServerPanelCommand
-from .plugin.core.panels import WindowPanelListener
 from .plugin.core.protocol import Location
-from .plugin.core.protocol import Response
-from .plugin.core.protocol import WorkspaceFolder
 from .plugin.core.registry import LspRecheckSessionsCommand
 from .plugin.core.registry import LspRestartServerCommand
 from .plugin.core.registry import windows
 from .plugin.core.sessions import AbstractPlugin
-from .plugin.core.sessions import method2attr
 from .plugin.core.sessions import register_plugin
-from .plugin.core.sessions import Session
 from .plugin.core.settings import client_configs
 from .plugin.core.settings import load_settings
 from .plugin.core.settings import unload_settings
 from .plugin.core.signature_help import LspSignatureHelpNavigateCommand
 from .plugin.core.signature_help import LspSignatureHelpShowCommand
 from .plugin.core.transports import kill_all_subprocesses
-from .plugin.core.types import ClientConfig
-from .plugin.core.typing import Any, Optional, List, Type, Callable, Dict, Tuple
+from .plugin.core.typing import Any, Optional, List, Type, Dict
 from .plugin.core.views import get_uri_and_position_from_location
 from .plugin.core.views import LspRunTextCommandHelperCommand
 from .plugin.documents import DocumentSyncListener
@@ -93,65 +85,6 @@ def _register_all_plugins() -> None:
         except NotImplementedError:
             continue
         register_plugin(plugin_class, notify_listener=False)
-    # TODO: Anything below here should be removed about a month after ST4 is released.
-    language_handler_classes = []  # type: List[Type[LanguageHandler]]
-    _get_final_subclasses(LanguageHandler.__subclasses__(), language_handler_classes)
-    for language_handler_class in language_handler_classes:
-        # Create an ephemeral plugin that stores an instance of the LanguageHandler as a class instance. Custom requests
-        # and notifications will work.
-        class LanguageHandlerTransition(AbstractPlugin):
-
-            handler = language_handler_class()
-
-            @classmethod
-            def name(cls) -> str:
-                return cls.handler.name  # type: ignore
-
-            @classmethod
-            def configuration(cls) -> Tuple[sublime.Settings, str]:
-                file_base_name = cls.name()
-                if file_base_name.startswith("lsp-"):
-                    file_base_name = "LSP-" + file_base_name[len("lsp-"):]
-                settings = sublime.load_settings("{}.sublime-settings".format(file_base_name))
-                cfg = cls.handler.config  # type: ignore
-                settings.set("command", cfg.command)
-                settings.set("settings", cfg.settings.get(None))
-                if isinstance(cfg.init_options, DottedDict):
-                    init_options = cfg.init_options.get()
-                elif isinstance(cfg.init_options, dict):
-                    init_options = cfg.init_options
-                else:
-                    init_options = {}
-                settings.set("initializationOptions", init_options)
-                settings.set("selector", cfg.selector)
-                settings.set("priority_selector", cfg.priority_selector)
-                return settings, "Packages/{0}/{0}.sublime-settings".format(file_base_name)
-
-            @classmethod
-            def can_start(cls, window: sublime.Window, initiating_view: sublime.View,
-                          workspace_folders: List[WorkspaceFolder], configuration: ClientConfig) -> Optional[str]:
-                if hasattr(cls.handler, 'on_start'):
-                    if not cls.handler.on_start(window):  # type: ignore
-                        return "{} cannot start".format(cls.name())
-                return None
-
-            def __init__(self, weaksession: 'weakref.ref[Session]') -> None:
-                super().__init__(weaksession)
-                if hasattr(self.handler, 'on_initialized'):
-                    self.handler.on_initialized(self)  # type: ignore
-
-            def on_notification(self, method: str, handler: Callable) -> None:
-                setattr(self, method2attr(method), handler)
-
-            def on_request(self, method: str, handler: Callable) -> None:
-                setattr(self, method2attr(method), handler)
-
-            def send_response(self, response: Response) -> None:
-                session = self.weaksession()
-                if session:
-                    session.send_response(response)
-
-        register_plugin(LanguageHandlerTransition, notify_listener=False)
 
 
 def _unregister_all_plugins() -> None:

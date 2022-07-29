85b9c7e3e6f59f2b1da7382c495560f8d1a4777b
Janos Wortmann <jwortmann@outlook.com>
2022-07-29 12:11:01 +0200

    Fixup

--- plugin/core/sessions.py
+++ plugin/core/sessions.py
@@ -254,9 +254,9 @@ def get_initialize_params(variables: Dict[str, str], workspace_folders: List[Wor
         },
         "signatureHelp": {
             "dynamicRegistration": True,
-            "activeParameterSupport": True,
             "contextSupport": True,
             "signatureInformation": {
+                "activeParameterSupport": True,
                 "documentationFormat": ["markdown", "plaintext"],
                 "parameterInformation": {
                     "labelOffsetSupport": True

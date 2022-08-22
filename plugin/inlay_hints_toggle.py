96ae243ff70c5fcd7596f44ee5417c5597b9b40e
Predrag Nikolic <predrag.nikolic@codetri.be>
2022-08-22 15:06:31 +0200

    rename get_target to get_view_or_window

--- plugin/inlay_hints_toggle.py
+++ plugin/inlay_hints_toggle.py
@@ -10,7 +10,7 @@ class InlayHints:
     global_show_inlay_hints = False
 
     @staticmethod
-    def get_target(v: sublime.View) -> Union[sublime.View, sublime.Window]:
+    def get_view_or_window(v: sublime.View) -> Union[sublime.View, sublime.Window]:
         if InlayHints.toggle_strategy == 'current_window':
             w = v.window()
             if not w:
@@ -22,13 +22,13 @@ class InlayHints:
     def are_enabled(v: sublime.View) -> bool:
         if InlayHints.toggle_strategy == 'all_windows':
             return InlayHints.global_show_inlay_hints
-        target = InlayHints.get_target(v)
-        return target.settings().get('lsp_show_inlay_hints') or userprefs().show_inlay_hints
+        view_or_window = InlayHints.get_view_or_window(v)
+        return view_or_window.settings().get('lsp_show_inlay_hints') or userprefs().show_inlay_hints
 
     @staticmethod
     def toggle(v: sublime.View) -> None:
         if InlayHints.toggle_strategy == 'all_windows':
             InlayHints.global_show_inlay_hints = not InlayHints.global_show_inlay_hints
             return
-        target = InlayHints.get_target(v)
-        target.settings().set('lsp_show_inlay_hints', not InlayHints.are_enabled(v))
+        view_or_window = InlayHints.get_view_or_window(v)
+        view_or_window.settings().set('lsp_show_inlay_hints', not InlayHints.are_enabled(v))

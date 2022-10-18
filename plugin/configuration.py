e1be509a5fe6c20a7607a79b9aebcb83f8f5541d
Rafał Chłodnicki <rchl2k@gmail.com>
2022-10-18 08:54:28 +0200

    fix issues with restarting servers (#2088)

--- plugin/configuration.py
+++ plugin/configuration.py
@@ -46,8 +46,6 @@ class LspDisableLanguageServerGloballyCommand(sublime_plugin.WindowCommand):
         if index > -1:
             config_name = self._items[index]
             client_configs.disable(config_name)
-            wm = windows.lookup(self.window)
-            sublime.set_timeout_async(lambda: wm._end_sessions_async(config_name))
 
 
 class LspDisableLanguageServerInProjectCommand(sublime_plugin.WindowCommand):

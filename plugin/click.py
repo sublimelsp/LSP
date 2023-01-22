d6ddc6ef1ed4c3444af9dd746e65247f3dc5753b
Предраг Николић <idmpepe@gmail.com>
2023-01-22 21:23:02 +0100

    refactor

--- plugin/click.py
+++ plugin/click.py
@@ -3,9 +3,7 @@ import sublime_plugin
 
 
 class LspClick(sublime_plugin.TextCommand):
-    def __init__(self, view: sublime.View) -> None:
-        super().__init__(view)
-        self.click_count = 0
+    click_count = 0
 
     def run(self, edit: sublime.Edit, count: int, command: str, args: dict) -> None:
         self.click_count += 1

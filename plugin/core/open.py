b88626b6216bd4bf1ffaf242f8836f3dd5fb49c4
Rafal Chlodnicki <rchl2k@gmail.com>
2023-01-22 17:09:08 +0100

    workaround View listeners not being attached for opened transient view

--- plugin/core/open.py
+++ plugin/core/open.py
@@ -12,6 +12,7 @@ from urllib.parse import unquote, urlparse
 import os
 import re
 import sublime
+import sublime_plugin
 import subprocess
 import webbrowser
 
@@ -86,8 +87,13 @@ def open_file(
     if view and _return_existing_view(flags, window.get_view_index(view)[0], window.active_group(), group):
         return Promise.resolve(view)
 
+    was_already_open = view is not None
     view = window.open_file(file, flags, group)
     if not view.is_loading():
+        if was_already_open and (flags & sublime.SEMI_TRANSIENT):
+            # work-around bug https://github.com/sublimehq/sublime_text/issues/2411 where transient view might not get
+            # its view listeners initialized.
+            sublime_plugin.check_view_event_listeners(view)  # type: ignore
         # It's already loaded. Possibly already open in a tab.
         return Promise.resolve(view)
 

8b28c7e9ee5bb4f5a482db4fd0a2ecbcee993cf4
Rafał Chłodnicki <rchl2k@gmail.com>
2023-02-26 21:25:39 +0100

    protect again invalid `auto_complete_triggers` values (#2209)

--- plugin/session_view.py
+++ plugin/session_view.py
@@ -149,7 +149,7 @@ class SessionView:
         '''Remove all of our modifications to the view's "auto_complete_triggers"'''
         triggers = settings.get(self.AC_TRIGGERS_KEY)
         if isinstance(triggers, list):
-            triggers = [t for t in triggers if self.session.config.name != t.get("server", "")]
+            triggers = [t for t in triggers if isinstance(t, dict) and self.session.config.name != t.get("server", "")]
             settings.set(self.AC_TRIGGERS_KEY, triggers)
 
     def _setup_auto_complete_triggers(self, settings: sublime.Settings) -> None:
@@ -170,6 +170,8 @@ class SessionView:
             new_triggers = []  # type: List[Dict[str, str]]
             name = self.session.config.name
             for trigger in triggers:
+                if not isinstance(trigger, dict):
+                    continue
                 if trigger.get("server", "") == name and trigger.get("registration_id", "") == registration_id:
                     continue
                 new_triggers.append(trigger)

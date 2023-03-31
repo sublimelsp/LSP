7fd594604da6317abc2b1c2f3a3d294fe77e9a6d
Rafal Chlodnicki <rchl2k@gmail.com>
2023-03-31 23:23:14 +0200

    combine elif and else blocks and use common logic

--- tests/test_completion.py
+++ tests/test_completion.py
@@ -708,7 +708,7 @@ class QueryCompletionsTests(CompletionsTestsBase):
         )
         check(
             resolve_support=False,
-            expected_regex=r"^f\(X&amp; x\) \| does things$",
+            expected_regex=r"^f\(X&amp; x\)$",
             label="f",
             label_details={"detail": "(X& x)", "description": "does things"}
         )
@@ -726,7 +726,7 @@ class QueryCompletionsTests(CompletionsTestsBase):
         )
         check(
             resolve_support=True,
-            expected_regex=r"^<a href='subl:lsp_run_text_command_helper {\S+}'>More</a> \| f\(X&amp; x\) \| does things$",  # noqa: E501
+            expected_regex=r"^<a href='subl:lsp_run_text_command_helper {\S+}'>More</a> \| f\(X&amp; x\)$",  # noqa: E501
             label="f",
             label_details={"detail": "(X& x)", "description": "does things"}
         )

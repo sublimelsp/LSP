964e5a88d57a04fa6464125e14d0cbb564bf14c1
Предраг Николић <idmpepe@gmail.com>
2023-04-09 10:53:55 +0200

    avoid call to make_link in format_completion
    instead just create a link fast with a string

--- plugin/completion.py
+++ plugin/completion.py
@@ -63,7 +63,7 @@ def format_completion(
         args = '{{"view_id":{},"command":"lsp_resolve_docs","args":{{"index":{},"session_name":"{}"}}}}'.format(
             view_id, index, session_name)
         href = 'subl:lsp_run_text_command_helper {}'.format(args)
-        details.append(make_link(href, 'More'))
+        details.append("<a href='{}'>More</a>".format(href))
     if lsp_label_detail and (lsp_label + lsp_label_detail).startswith(lsp_filter_text):
         if lsp_label_detail[0].isalnum() and lsp_label.startswith(lsp_filter_text):
             # labelDetails.detail is likely a type annotation

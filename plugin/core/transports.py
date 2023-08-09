078399ab5b0a1a57e218f0543127aadabcf1528c
Marek Budik <mbudik@protonmail.com>
2023-08-09 23:05:16 +0200

    whitespace

--- plugin/core/transports.py
+++ plugin/core/transports.py
@@ -137,7 +137,6 @@ class ProcessTransport(Transport[T]):
         self.close()
         self._join_thread(self._writer_thread)
         self._join_thread(self._reader_thread)
-
         if self._stderr_thread is not None:
             self._join_thread(self._stderr_thread)
 

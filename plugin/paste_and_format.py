2c72702f1156eecf97e2f043a7799ea8096d9fd3
Предраг Николић <idmpepe@gmail.com>
2023-08-17 11:02:35 +0200

    support copy/paste for multiple cursors
    note: some servers don't support textDocument/rangesFormatting
    so when there are multiple selections, text will not be formatted in that case

--- plugin/paste_and_format.py
+++ plugin/paste_and_format.py
@@ -12,7 +12,15 @@ class LspPasteAndFormatCommand(LspTextCommand):
         region = first_selection_region(self.view)
         if region is None:
             return
-        self.view.replace(edit, region, clipboard_text)
+        sel = self.view.sel()
+        number_of_cursors = len(sel)
+        split_clipboard_text = clipboard_text.split('\n')
+        for index, region  in enumerate(sel):
+            insert_text = clipboard_text
+            # mimic copy/paste behavior of ST when there are multiple cursors
+            if len(split_clipboard_text) == number_of_cursors:
+                insert_text = split_clipboard_text[index]
+            self.view.replace(edit, region, insert_text)
         # if we don't delay execution self.format with set_timeout_async,
         # on_text_changed_async will not pick up the change
         # thus a call to listener.purge_changes_async will not see new text changes.

772a54c9e46ea86e27f13a4f7ff4854a3f1a730e
Janos Wortmann <jwortmann@outlook.com>
2023-10-23 17:37:23 +0200

    Update note

--- plugin/core/constants.py
+++ plugin/core/constants.py
@@ -1,4 +1,5 @@
-# TODO: Move all constants into this file, so that all other modules can import them without causing import loops
+# TODO: Move all constants which are shared by multiple modules into this file, so that they can be imported without
+# causing import loops
 
 HOVER_HIGHLIGHT_KEY = "lsp_hover_highlight"
 HOVER_PROVIDER_COUNT_KEY = "lsp_hover_provider_count"

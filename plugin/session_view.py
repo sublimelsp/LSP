e3b277952af4a7366c15c253420f5c54ec2ba61d
Janos Wortmann <jwortmann@outlook.com>
2023-10-30 10:33:50 +0100

    Unused import

--- plugin/session_view.py
+++ plugin/session_view.py
@@ -13,7 +13,6 @@ from .core.protocol import Notification
 from .core.protocol import Request
 from .core.sessions import AbstractViewListener
 from .core.sessions import Session
-from .core.settings import globalprefs
 from .core.settings import userprefs
 from .core.typing import Any, Iterable, List, Tuple, Optional, Dict, Generator
 from .core.views import DIAGNOSTIC_SEVERITY

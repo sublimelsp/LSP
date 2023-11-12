2769e442bb54aa1ad1ec925eb83c12c35136384f
Janos Wortmann <jwortmann@outlook.com>
2023-11-12 16:11:09 +0100

    Add test for scheme parsing

--- tests/test_url.py
+++ tests/test_url.py
@@ -71,3 +71,9 @@ class MultiplatformTests(unittest.TestCase):
         view.buffer_id = unittest.mock.MagicMock(return_value=42)
         uri = view_to_uri(view)
         self.assertEqual(uri, "buffer:42")
+
+    def test_parse_uri(self):
+        scheme, _ = parse_uri("buffer:42")
+        self.assertEqual(scheme, "buffer")
+        scheme, _ = parse_uri("www.example.com/foo:bar")
+        self.assertEqual(scheme, "")

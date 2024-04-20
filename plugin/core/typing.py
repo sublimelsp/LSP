ac2ee2b7cd33854375c879d883bef5d5b81dfaa7
Jack Cherng <jfcherng@gmail.com>
2024-04-21 04:06:50 +0800

    chore: reduce amount of "# noqa: 401"s
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/core/typing.py
+++ plugin/core/typing.py
@@ -1,39 +1,39 @@
 import sys
 from enum import Enum, IntEnum, IntFlag  # noqa: F401
 from typing import (  # noqa: F401
-    IO,  # noqa: F401
-    TYPE_CHECKING,  # noqa: F401
-    Any,  # noqa: F401
-    Callable,  # noqa: F401
-    Deque,  # noqa: F401
-    Dict,  # noqa: F401
-    Generator,  # noqa: F401
-    Generic,  # noqa: F401
-    Iterable,  # noqa: F401
-    Iterator,  # noqa: F401
-    List,  # noqa: F401
-    Literal,  # noqa: F401
-    Mapping,  # noqa: F401
-    Optional,  # noqa: F401
-    Protocol,  # noqa: F401
-    Sequence,  # noqa: F401
-    Set,  # noqa: F401
-    Tuple,  # noqa: F401
-    Type,  # noqa: F401
-    TypedDict,  # noqa: F401
-    TypeVar,  # noqa: F401
-    Union,  # noqa: F401
-    cast,  # noqa: F401
-    final,  # noqa: F401
+    IO,
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Deque,
+    Dict,
+    Generator,
+    Generic,
+    Iterable,
+    Iterator,
+    List,
+    Literal,
+    Mapping,
+    Optional,
+    Protocol,
+    Sequence,
+    Set,
+    Tuple,
+    Type,
+    TypedDict,
+    TypeVar,
+    Union,
+    cast,
+    final,
 )
 
 if sys.version_info >= (3, 11):
     from enum import StrEnum  # noqa: F401
-    from typing import (
-        NotRequired,  # noqa: F401
-        ParamSpec,  # noqa: F401
-        Required,  # noqa: F401
-        TypeGuard,  # noqa: F401
+    from typing import (  # noqa: F401
+        NotRequired,
+        ParamSpec,
+        Required,
+        TypeGuard,
     )
 else:
     _T = TypeVar("_T")

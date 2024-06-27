437659fd4e865f7e5c4c3e22f72d93ea788f8fa9
Предраг Николић <idmpepe@gmail.com>
2024-06-27 23:39:40 +0200

    simpler conditions

--- plugin/rename_file.py
+++ plugin/rename_file.py
@@ -39,9 +39,9 @@ class LspRenameFileCommand(LspWindowCommand):
     def is_visible(self, dirs=None, files=None):
         if dirs is None and files is None:
             return True  # show 'LSP: Rename File' in command palette
-        if dirs is not None:
+        if dirs:
             return len(dirs) == 1  # show 'LSP: Rename Folder' in sidebar
-        if files is not None:
+        if files:
             return len(files) == 1  # show 'LSP: Rename File' in sidebar
         return False
 

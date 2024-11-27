89c4bd9542f29ea58c442506353e95d22b66dc0c
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-27 15:40:21 +0700

    remove severity level from log messages

--- plugin/core/logging_notify.py
+++ plugin/core/logging_notify.py
@@ -3,11 +3,10 @@ import sublime
 from .settings import userprefs
 
 
-def notify_error(window: sublime.Window | None, message: str, status_message: str | None = None,
-                 severe: bool = True) -> None:
+def notify_error(window: sublime.Window | None, message: str, status_message: str | None = None) -> None:
     """Pick either of the 2 ways to show a user error notification message:
       - via a detailed console message and a short status message
-      - via a blocking error modal dialog (with severe=false non-error modal dialog)"""
+      - via a blocking error modal dialog"""
     if not window:
         return
     if status_message is None:
@@ -16,7 +15,4 @@ def notify_error(window: sublime.Window | None, message: str, status_message: st
         window.status_message(status_message)
         print("LSP: " + message)
     else:
-        if severe:
-            sublime.error_message(message)
-        else:
-            sublime.message_dialog(message)
+        sublime.error_message(message)

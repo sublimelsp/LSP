d61affa2538ca03d63cee73775adc09eb194198b
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-27 12:57:32 +0700

    update type/var name

--- plugin/core/logging.py
+++ plugin/core/logging.py
@@ -41,31 +41,31 @@ def printf(*args: Any, prefix: str = 'LSP') -> None:
     print(prefix + ":", *args)
 
 
-def notify(window: Optional[sublime.Window], message: str, status: str = 'LSP: see console log…') -> None:
+def notify(window: sublime.Window | None, message: str, status_message: str = 'LSP: see console log…') -> None:
     """Pick either of the 2 ways to show a user notification message:
       - via a detailed console message and a short status message
       - via a blocking modal dialog"""
     from .settings import userprefs
     if userprefs().suppress_error_dialogs:
         if window:
-            window.status_message(status)
+            window.status_message(status_message)
         else:
-            sublime.status_message(status)
+            sublime.status_message(status_message)
         print(message)
     else:
         sublime.message_dialog(message)
 
 
-def notify_error(window: Optional[sublime.Window], message: str, status: str = '❗LSP: see console log…') -> None:
+def notify_error(window: sublime.Window | None, message: str, status_message: str = '❗LSP: see console log…') -> None:
     """Pick either of the 2 ways to show a user error notification message:
       - via a detailed console message and a short status message
       - via a blocking error modal dialog"""
     from .settings import userprefs
     if userprefs().suppress_error_dialogs:
         if window:
-            window.status_message(status)
+            window.status_message(status_message)
         else:
-            sublime.status_message(status)
+            sublime.status_message(status_message)
         print(message)
     else:
         sublime.error_message(message)

3832fb79ab53b3b26d1e6bf837839a397f136bc4
95833 <987004590@qq.com>
2024-12-08 03:11:46 +0800

    Update url.py
    Adjust path.lstrip("\\") to path[1:] to fit WSL path that starts with "\\wsl.local"

--- plugin/core/url.py
+++ plugin/core/url.py
@@ -54,7 +54,8 @@ def parse_uri(uri: str) -> tuple[str, str]:
         path = url2pathname(parsed.path)
         if os.name == 'nt':
             netloc = url2pathname(parsed.netloc)
-            path = path[1:]
+            if path[0] == '\\':
+                path = path[1:]
             path = re.sub(r"^/([a-zA-Z]:)", r"\1", path)  # remove slash preceding drive letter
             path = re.sub(r"^([a-z]):", _uppercase_driveletter, path)
             if netloc:

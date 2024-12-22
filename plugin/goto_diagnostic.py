7ed9c77368ea597d2d655776cc886b602aa360ff
95833 <987004590@qq.com>
2024-12-22 19:23:17 +0800

    Update goto_diagnostic.py

--- plugin/goto_diagnostic.py
+++ plugin/goto_diagnostic.py
@@ -265,7 +265,7 @@ def open_location(
     session: Session, location: Location, flags: sublime.NewFileFlags = sublime.NewFileFlags.NONE, group: int = -1
 ) -> sublime.View:
     uri, position = get_uri_and_position_from_location(location)
-    file_name = to_encoded_filename(session.config.map_uri_on_payload(uri, is_from_client_to_server=False), position)
+    file_name = to_encoded_filename(session.config.map_server_uri_to_client_path(uri), position)
     return session.window.open_file(file_name, flags=flags | sublime.NewFileFlags.ENCODED_POSITION, group=group)
 
 

baed32c9920316e20a2ea5778d9c82ababb1229e
Предраг Николић <idmpepe@gmail.com>
2025-09-13 11:42:38 +0200

    remove .replace(' ', ' ') as I cannot seem to find the example hover content on why I've added it...
    For now I will remove it, if there is a need it can easily be brought back

--- plugin/core/views.py
+++ plugin/core/views.py
@@ -860,7 +860,7 @@ def copy_text_html(html_content: str, copy_text: str | MarkupContent | MarkedStr
     return f"""<a title="Click to Copy"
        style='text-decoration: none; display: block; color: inherit'
        href='{sublime.command_url('lsp_copy_text', {
-        'text': copy_text.replace(' ', ' ')
+        'text': copy_text
        })}'>{html_content}</a>"""
 
 

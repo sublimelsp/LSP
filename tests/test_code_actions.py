ed9b82d6c003b9f1ed714319e391c345fa160a2b
Rafal Chlodnicki <rchl2k@gmail.com>
2025-10-20 20:22:45 +0200

    Revert "random change"
    This reverts commit fe94c038cfceda8435f561d2441305b238d061af.

--- tests/test_code_actions.py
+++ tests/test_code_actions.py
@@ -239,7 +239,6 @@ class CodeActionMatchingTestCase(unittest.TestCase):
         expected: list[str] = []
         self.assertEqual(actual, expected)
 
-
     def test_matches_exact_action(self) -> None:
         actual = get_matching_on_save_kinds({'a.b': True}, ['a.b'])
         expected = ['a.b']

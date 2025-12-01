4616c1a93d0d3515ffc8c6f272e130d8b572d931
Предраг Николић <idmpepe@gmail.com>
2025-12-01 21:17:02 +0100

    Reapply "Merge branch 'main' into add-will-rename-and-did-rename" because it bring back the problem I faced with project_a
    This reverts commit 0521ed23b4b72cbf2396a43b362b676b70a7a7b9.

--- plugin/session_buffer.py
+++ plugin/session_buffer.py
@@ -158,8 +158,6 @@ class SessionBuffer:
         self._dynamically_registered_commands: dict[str, list[str]] = {}
         self._supported_commands: set[str] = set()
         self._update_supported_commands()
-        # set_timeout_async is required to ensure that the SessionView is initialized before running requests
-        sublime.set_timeout_async(lambda: self._check_did_open(view))
 
     @property
     def session(self) -> Session:
@@ -176,6 +174,9 @@ class SessionBuffer:
     @property
     def last_synced_version(self) -> int:
         return self._last_synced_version
+
+    def on_session_view_initialized(self, view: sublime.View) -> None:
+        self._check_did_open(view)
 
     def _check_did_open(self, view: sublime.View) -> None:
         if not self.opened and self.should_notify_did_open():
@@ -446,10 +447,12 @@ class SessionBuffer:
         # Prefer active view if possible
         active_view = self.session.window.active_view()
         for sv in self.session_views:
-            if sv.view == active_view:
+            if sv.view == active_view and sv.view.is_valid():
                 return active_view
         for sv in self.session_views:
-            return sv.view
+            if sv.view.is_valid():
+                return sv.view
+        return None
 
     def _if_view_unchanged(self, f: Callable[[sublime.View, Any], None], version: int) -> CallableWithOptionalArguments:
         """

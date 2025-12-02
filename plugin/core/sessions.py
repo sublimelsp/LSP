cf03a2e06a56a2b516467dc70c175e1ee045b771
Rafal Chlodnicki <rchl2k@gmail.com>
2025-12-02 19:22:19 +0100

    i'm not really testing it :)

--- plugin/core/sessions.py
+++ plugin/core/sessions.py
@@ -2481,7 +2481,7 @@ class Session(TransportCallbacks):
         return (promise, request_id)
 
     def cancel_request_async(self, request_id: int, *, ignore_response: bool = True) -> None:
-        if request_id in self._response_handlers.get(request_id):
+        if request_id in self._response_handlers:
             self.send_notification(Notification("$/cancelRequest", {"id": request_id}))
             request, _, _ = self._response_handlers[request_id]
             self._invoke_views(request, "on_request_canceled_async", request_id)

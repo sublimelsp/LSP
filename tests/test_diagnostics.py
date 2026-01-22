816865963f2bdd2a2e1850422cb95998a55c6911
Rafal Chlodnicki <rchl2k@gmail.com>
2026-01-22 18:15:47 +0100

    update test

--- tests/test_diagnostics.py
+++ tests/test_diagnostics.py
@@ -55,10 +55,12 @@ class DiagnosticsTestCase(TextDocumentTestCase):
             "textDocument/publishDiagnostics",
             create_test_diagnostics([('error', Point(0, 0), Point(0, 11))])
         )
+        session_buffer = self.session.get_session_buffer_for_uri_async(TEST_FILE_URI)
+        self.assertEqual(len(session_buffer.diagnostics), 1)
+
         sublime.set_timeout_async(insert_text_and_clear_diagnostics_async)
         yield AWAIT_WORKER
         # Just a dummy wait to ensure that the `textDocument/publishDiagnostics` triggered from async thread
         # is processed since we can't await it there.
         yield from self.await_client_notification('$/dummy', [])
-        session_buffer = self.session.get_session_buffer_for_uri_async(TEST_FILE_URI)
         self.assertEqual(len(session_buffer.diagnostics), 0)

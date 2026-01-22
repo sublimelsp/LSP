fd737190f170127d6540ea0febdd85de924c9f56
Rafal Chlodnicki <rchl2k@gmail.com>
2026-01-22 17:22:44 +0100

    test

--- tests/test_diagnostics.py
+++ tests/test_diagnostics.py
@@ -43,9 +43,9 @@ class DiagnosticsTestCase(TextDocumentTestCase):
         #
         # Verify that the diagnostics are properly cleared.
 
-        def insert_text_and_clear_diagnostics() -> None:
+        def insert_text_and_clear_diagnostics() -> Generator:
             # Don't wait for result - trigger edit immediately before receiving publishDiagnostics.
-            next(self.await_client_notification("textDocument/publishDiagnostics", create_test_diagnostics([])))
+            yield from self.await_client_notification("textDocument/publishDiagnostics", create_test_diagnostics([]))
             self.insert_characters('// anything')
 
         self.insert_characters('const x = 1')

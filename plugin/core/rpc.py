f742d43cf4f0eab21d0b139b3875939db4bfb89d
Rafal Chlodnicki <rchl2k@gmail.com>
2026-02-20 21:03:54 +0100

    Enable ruff's required-imports

--- plugin/core/rpc.py
+++ plugin/core/rpc.py
@@ -1,3 +1,5 @@
 # only used for LSP-* packages out in the wild that now import from "private" modules
 # TODO: Announce removal and remove this import
+from __future__ import annotations
+
 from .types import method2attr  # noqa

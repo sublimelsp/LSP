46102ce814e5fec4738c173f7af969f9f44e4bd7
Rafal Chlodnicki <rchl2k@gmail.com>
2026-03-16 21:09:20 +0100

    fix

--- plugin/core/views.py
+++ plugin/core/views.py
@@ -894,7 +894,7 @@ def format_diagnostic_for_html(config: ClientConfig, diagnostic: Diagnostic, bas
     if related_infos := diagnostic.get("relatedInformation"):
         info = "<br>".join(_format_diagnostic_related_info(config, info, base_dir) for info in related_infos)
         html += '<br>' + _html_element("pre", info, class_name="related_info", escape=False)
-    severity_class = DIAGNOSTIC_STYLES[diagnostic_severity(diagnostic)].kind
+    severity_class = DIAGNOSTIC_STYLES[diagnostic_severity(diagnostic)].css_class
     return _html_element("pre", html, class_name=severity_class, escape=False)
 
 

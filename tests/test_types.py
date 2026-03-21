6b96e28bdc2dab717eae20158f88d392244ce7f3
Rafal Chlodnicki <rchl2k@gmail.com>
2026-03-21 14:30:31 +0100

    fix C405 - https://docs.astral.sh/ruff/rules/unnecessary-literal-set/

--- tests/test_types.py
+++ tests/test_types.py
@@ -12,28 +12,28 @@ class TestDiff(unittest.TestCase):
 
     def test_add(self) -> None:
         added, removed = diff(("a", "b", "c"), ("a", "b", "c", "d"))
-        self.assertEqual(added, set(("d",)))
+        self.assertEqual(added, {"d"})
         self.assertFalse(removed)
 
     def test_remove(self) -> None:
         added, removed = diff(("a", "b", "c"), ("c", "b"))
         self.assertFalse(added)
-        self.assertEqual(removed, set(("a",)))
+        self.assertEqual(removed, {"a"})
 
     def test_add_and_remove(self) -> None:
         added, removed = diff(("a", "b", "c"), ("c", "d"))
-        self.assertEqual(added, set(("d",)))
-        self.assertEqual(removed, set(("a", "b")))
+        self.assertEqual(added, {"d"})
+        self.assertEqual(removed, {"a", "b"})
 
     def test_with_sets(self) -> None:
-        added, removed = diff(set(("a", "b", "c")), ("x", "y", "z"))
-        self.assertEqual(added, set(("x", "y", "z")))
-        self.assertEqual(removed, set(("a", "b", "c")))
+        added, removed = diff({"a", "b", "c"}, ("x", "y", "z"))
+        self.assertEqual(added, {"x", "y", "z"})
+        self.assertEqual(removed, {"a", "b", "c"})
 
     def test_with_more_sets(self) -> None:
-        added, removed = diff(set(("a", "b")), set(("b", "c")))
-        self.assertEqual(added, set(("c",)))
-        self.assertEqual(removed, set(("a",)))
+        added, removed = diff({"a", "b"}, {"b", "c"})
+        self.assertEqual(added, {"c"})
+        self.assertEqual(removed, {"a"})
 
     def test_completely_new(self) -> None:
         new = {"ocaml", "polymer-ide", "elixir-ls", "jdtls", "dart", "reason", "golsp", "clangd", "pwsh", "vhdl_ls"}

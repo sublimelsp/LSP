8d81f42e4721accbdfd532346367b1b5d03bef92
Rafal Chlodnicki <rchl2k@gmail.com>
2026-04-19 00:06:44 +0200

    simplify

--- plugin/core/views.py
+++ plugin/core/views.py
@@ -917,12 +917,12 @@ def format_diagnostic_for_html(
     content = _format_diagnostic_message(view, message)
     code = diagnostic.get("code")
     source = diagnostic.get("source")
-    copy_text = f'{raw_message} '.replace(' ', ' ')
+    copy_text = f'{raw_message}'.replace(' ', ' ')
     if source or code is not None:
         meta_info = ""
         if source:
             meta_info += text2html(source)
-            copy_text += f'({source})' if code is None else f'({source}[{code}])'
+            copy_text += f' ({source})' if code is None else f' ({source}[{code}])'
         if code is not None:
             if code_description := diagnostic.get("codeDescription"):
                 href = code_description["href"]
@@ -931,7 +931,7 @@ def format_diagnostic_for_html(
                 meta_info += f'({text2html(str(code))})'
         content += " " + _html_element("span", meta_info, class_name="color-muted", escape=False)
     content += f"""<a class='copy-icon' title='Copy to clipboard' href='{sublime.command_url(
-        'lsp_copy_text', {'text': copy_text.strip()}
+        'lsp_copy_text', {'text': copy_text}
     )}'>⧉</a>"""
     if related_infos := diagnostic.get("relatedInformation"):
         info = "<br>".join(_format_diagnostic_related_info(config, info, base_dir) for info in related_infos)

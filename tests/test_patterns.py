cf4b623cc6ee833a396806d8395f771ceee597f0
Rafal Chlodnicki <rchl2k@gmail.com>
2026-05-03 10:44:41 +0200

    widnows fixes

--- tests/test_patterns.py
+++ tests/test_patterns.py
@@ -50,8 +50,6 @@ class PatternToGlobTests(unittest.TestCase):
 
 @unittest.skipUnless(sys.platform.startswith("win"), "requires Windows")
 class PatternToGlobWindowsTests(unittest.TestCase):
-    # Unlike Unix roots (which start with /), Windows roots do not start with /,
-    # so posixpath.join produces a path that triggers the **/ prefix in the output.
 
     def test_project_relative_directory_pattern_forward_slash_root(self) -> None:
         self.assertEqual(
@@ -71,12 +69,12 @@ class PatternToGlobWindowsTests(unittest.TestCase):
     def test_project_relative_directory_pattern_backslash_root(self) -> None:
         self.assertEqual(
             sublime_pattern_to_glob('//foo', is_directory_pattern=True, root_path=r'C:\Users\me\project'),
-            '**/C:/Users/me/project/foo/**')
+            'C:/Users/me/project/foo/**')
 
     def test_project_relative_file_pattern_backslash_root(self) -> None:
         self.assertEqual(
             sublime_pattern_to_glob('//*.pyo', is_directory_pattern=False, root_path=r'C:\Users\me\project'),
-            '**/C:/Users/me/project/**/*.pyo')
+            'C:/Users/me/project/**/*.pyo')
 
     def test_project_relative_without_root_path(self) -> None:
         self.assertEqual(sublime_pattern_to_glob('//foo', is_directory_pattern=True), '//foo/**')

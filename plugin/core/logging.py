68b8945b1f1b95eb586d393f1e608fde58d4ed9d
Raoul Wols <r@primef.actor>
2026-05-16 14:31:22 +0200

    Add function: exceptions_log
    Takes a list of exceptions and prints all of them.
    
    Also expand the `trace` function so it can print stack traces and
    optionally some values.

--- plugin/core/logging.py
+++ plugin/core/logging.py
@@ -20,7 +20,7 @@ def debug(*args: Any) -> None:
         printf(*args)
 
 
-def trace() -> None:
+def trace(print_callstack: bool = False, **values: Any) -> None:
     current_frame = inspect.currentframe()
     if current_frame is None:
         debug("TRACE (unknown frame)")
@@ -29,12 +29,22 @@ def trace() -> None:
     file_name, line_number, function_name, _, _ = inspect.getframeinfo(previous_frame)  # type: ignore
     file_name = file_name[len(ST_PACKAGES_PATH) + len("/LSP/"):]
     debug(f"TRACE {threading.current_thread().name:<16} {function_name:<32} {file_name}:{line_number}")
+    if print_callstack:
+        for frame in traceback.extract_stack():
+            debug(f"TRACE {frame.filename}:{frame.lineno} in {frame.name}")
+    for k, v in values.items():
+        debug(f"TRACE {k}={v}")
 
 
 def exception_log(message: str, ex: BaseException) -> None:
     print(message)
     ex_traceback = ex.__traceback__
     print(''.join(traceback.format_exception(ex.__class__, ex, ex_traceback)))
+
+
+def exceptions_log(message: str, exs: list[Exception]) -> None:
+    for ex in exs:
+        exception_log(message, ex)
 
 
 def printf(*args: Any, prefix: str = 'LSP') -> None:

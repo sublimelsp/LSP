927242ff6730d06bb3b38a3222c7b640ee259ccd
dontwanttothink <barquean.09excavacion@icloud.com>
2026-07-24 07:17:49 -0500

    Alphabetical order

--- plugin/core/constants.py
+++ plugin/core/constants.py
@@ -312,8 +312,8 @@ LANGUAGE_IDENTIFIERS: dict[str, str] = {
     "text.html.markdown": LanguageKind.Markdown,
     "text.html.markdown.rmarkdown": LanguageKind.R,  # https://github.com/REditorSupport/sublime-ide-r
     "text.html.rails": "erb",
-    "text.html.vue": "vue",
     "text.html.svelte": "svelte",
+    "text.html.vue": "vue",
     "text.jinja": LanguageKind.HTML,  # https://github.com/Sublime-Instincts/BetterJinja
     "text.plain": "plaintext",
     "text.plist": LanguageKind.XML,  # https://bitbucket.org/fschwehn/sublime_plist

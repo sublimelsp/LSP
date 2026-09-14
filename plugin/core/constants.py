2732df47478e727dd117da3f02615457c6ec865a
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-09-14 19:00:13 +0200

    Add language IDs for glimmer-js and glimmer-ts (#3028)
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- plugin/core/constants.py
+++ plugin/core/constants.py
@@ -287,6 +287,8 @@ LANGUAGE_IDENTIFIERS: dict[str, str] = {
     "source.cs": LanguageKind.CSharp,
     "source.dosbatch": LanguageKind.WindowsBat,
     "source.fixedform-fortran": "fortran",  # https://packagecontrol.io/packages/Fortran
+    "source.gjs": "glimmer-js",
+    "source.gts": "glimmer-ts",
     "source.js": LanguageKind.JavaScript,
     "source.js.react": LanguageKind.JavaScriptReact,  # https://github.com/Thom1729/Sublime-JS-Custom
     "source.json-tmlanguage": "jsonc",  # https://github.com/SublimeText/PackageDev

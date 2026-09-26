6601ee78778042832da448ad65d0c325ee6b91f2
Raoul Wols <r@primef.actor>
2026-09-26 17:32:14 +0200

    Revert "Maybe fix CI failure on macOS?"
    This reverts commit f0f037f15268ae3161b763c00320f858c8886be9.

--- plugin/core/transports.py
+++ plugin/core/transports.py
@@ -595,7 +595,7 @@ async def _start_subprocess(
 
 def _find_free_port() -> int:
     with contextlib.closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(("localhost", 0))
+        s.bind(("", 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
 
